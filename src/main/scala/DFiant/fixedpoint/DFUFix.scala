dda587439e9bd8a35314b738235f3e9a2236b039
Oron Port <soronpo@campus.technion.ac.il>
2017-10-02 02:40:06 +0300

    Best DFUFix so far

--- src/main/scala/DFiant/fixedpoint/DFUFix.scala
+++ src/main/scala/DFiant/fixedpoint/DFUFix.scala
@@ -4,11 +4,15 @@ import DFiant.core._
 import singleton.ops._
 import singleton.twoface._
 
-abstract class DFUFix[IWL, FWL] extends DFAny.ValW[IWL + FWL, DFUFix[IWL, FWL], DFUFix.Var[IWL, FWL]] {
+
+//DFUFix can be derived from DFStruct, however to increase (Scala) compile-time safety, we rather
+//define local field aliasing manually
+trait DFUFix[IWL, FWL] extends DFAny.ValW[IWL + FWL, DFUFix[IWL, FWL], DFUFix.Var[IWL, FWL]] {
   val iwl : TwoFace.Int[IWL]
   val fwl : TwoFace.Int[FWL]
 
-//  def fw(implicit tfc : TwoFace.Int.Shell2[-,FWL,Int,1,Int]) = protBits(tfc(fwl,1), 0)
+  val iw : TBits[IWL]
+  val fw : TBits[FWL]
   def newEmptyDFVar: TVar = DFUFix.create(iwl, fwl)
   def dfTypeName: String = "DFUFix"
 }
@@ -21,8 +25,11 @@ object DFUFix {
     val width = TwoFace.Int.create[Width](_iwl + _fwl)
     val iwl = _iwl
     val fwl = _fwl
+    val iw : TBits[IWL] = protBits(_fwl+_iwl-1, _fwl).asInstanceOf[TBits[IWL]]
+    val fw : TBits[FWL] = protBits(_fwl-1, 0).asInstanceOf[TBits[FWL]]
   }
 
   def apply[IWL, FWL](_iwl : TwoFace.Int[IWL], _fwl : TwoFace.Int[FWL]) = create(_iwl, _fwl)
+  implicit def apply[IWL, FWL](implicit _iwl : TwoFace.Int[IWL], _fwl : TwoFace.Int[FWL], di: DummyImplicit) = create(_iwl, _fwl)
 }
 
