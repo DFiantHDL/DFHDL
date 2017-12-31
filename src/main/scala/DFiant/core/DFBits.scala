25ac5748e691a0dd6a0393a70640db73baf68862
Oron Port <soronpo@campus.technion.ac.il>
2018-01-01 00:42:20 +0200

    Adding init

--- src/main/scala/DFiant/core/DFBits.scala
+++ src/main/scala/DFiant/core/DFBits.scala
@@ -59,10 +59,12 @@ trait DFBits[W] extends DFAny.Val[W, TokenBits, DFBits[W], DFBits.Var[W]] {
 
   //////////////////////////////////////////////////////////////////////////
 
-  def init(updatedInit : Seq[TToken]) : TAlias = {
-    DFBits.alias(this, width, 0, 0, updatedInit).asInstanceOf[TAlias]
-  }
+  //////////////////////////////////////////////////////////////////////////
+  // Init (for use with Prev)
+  //////////////////////////////////////////////////////////////////////////
+  def init(updatedInit : Seq[TToken]) : TAlias = DFBits.alias(this, width, 0, 0, updatedInit).asInstanceOf[TAlias]
   def init(args : Int*)(implicit di : DummyImplicit) : TAlias = init(args.map(a => TokenBits.fromNum(width, a)))
+  //////////////////////////////////////////////////////////////////////////
 
 
   //  def extBy(numOfBits : Int)    : TBits[WUnsafe] = (DFBits.Unsafe(width + numOfBits) := this).asInstanceOf[TBits[WUnsafe]]
