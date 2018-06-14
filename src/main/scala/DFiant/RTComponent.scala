f7b8ece34e15d1f1b379cba6f1c020cfef854e81
Oron Port <soronpo@campus.technion.ac.il>
2018-06-14 11:31:58 +0300

    Discovered RTComponent

--- src/main/scala/DFiant/RTComponent.scala
+++ src/main/scala/DFiant/RTComponent.scala
@@ -13,6 +13,14 @@ abstract class RTComponent(implicit dsn : DFDesign, n : NameIt) extends DFInterf
 
   final val id = dsn.newRTComponentGetID(this)
 
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Naming
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  override protected def nameDefault: String = n.value
+  final lazy val fullName : String = s"${dsn.fullName}.$name"
+  override def toString: String = s"$fullName : $typeName"
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
   final protected[DFiant] lazy val init : Unit = {
     //set Output Ports Dependency
     portNodes.map(pn => pn.dfport).filter(p => p.dir.isOut).foreach(p => p.setComponentDependency(this))
