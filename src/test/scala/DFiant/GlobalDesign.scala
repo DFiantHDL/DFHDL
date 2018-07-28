14366df41b7a83d744e5b390872375cac249f864
Oron Port <soronpo@campus.technion.ac.il>
2018-07-28 05:20:46 +0300

    Simplifying GlobalDesign

--- src/test/scala/DFiant/GlobalDesign.scala
+++ src/test/scala/DFiant/GlobalDesign.scala
@@ -3,33 +3,16 @@ package DFiant
 import DFiant.basiclib._
 import DFiant.internals._
 
-object DFGlobalLib extends DFBasicLib {
-  import DFComponent.Implementation
-  implicit def `evU+U`(implicit ctx : DFAny.Op.Context) : Implementation[`U+U`] = ifc => {}
-  implicit def `evU-U`(implicit ctx : DFAny.Op.Context) : Implementation[`U-U`] = ifc => {}
-  implicit def `evU*U`(implicit ctx : DFAny.Op.Context) : Implementation[`U*U`] = ifc => {}
-
-  implicit def `evU==U`(implicit ctx : DFAny.Op.Context) : Implementation[`U==U`] = ifc => {}
-  implicit def `evU!=U`(implicit ctx : DFAny.Op.Context) : Implementation[`U!=U`] = ifc => {}
-  implicit def `evU<U`(implicit ctx : DFAny.Op.Context) : Implementation[`U<U`] = ifc => {}
-  implicit def `evU>U`(implicit ctx : DFAny.Op.Context) : Implementation[`U>U`] = ifc => {}
-  implicit def `evU<=U`(implicit ctx : DFAny.Op.Context) : Implementation[`U<=U`] = ifc => {}
-  implicit def `evU>=U`(implicit ctx : DFAny.Op.Context) : Implementation[`U>=U`] = ifc => {}
-
-  implicit def `evE==E`[E <: Enum](implicit ctx : DFAny.Op.Context) : Implementation[`E==E`[E]] = ifc => {}
-  implicit def `evE!=E`[E <: Enum](implicit blk : DFAny.Op.Context) : Implementation[`E!=E`[E]] = ifc => {}
-}
-
 object GlobalDesignName extends NameIt {
   override val value: String = "???"
 }
 
 object GlobalContext extends DFDesign.Context {
   override val owner: Option[DFBlock] = None
-  override val basicLib: DFBasicLib = DFGlobalLib
+  override val basicLib: DFBasicLib = psuedoVendor.family.device.basicLib
   override val n: NameIt = GlobalDesignName
 }
 
 object GlobalDesign extends DFDesign()(GlobalContext) {
-  override implicit val protChildOwner = this
+  override implicit val protChildOwner = this //Make it public for the global design
 }
