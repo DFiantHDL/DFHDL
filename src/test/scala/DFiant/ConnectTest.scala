e5c90be27b5cae1b8c2b09821ad01b0e10af3804
Oron Port <soronpo@campus.technion.ac.il>
2018-07-31 01:54:54 +0300

    Tests

--- src/test/scala/DFiant/ConnectTest.scala
+++ src/test/scala/DFiant/ConnectTest.scala
@@ -7,12 +7,39 @@ import psuedoVendor.family.device._
 
 class ConnectTest extends Properties("ConnectTest") {
   trait IODesignConn1 extends DFDesign {
-    val i = DFUInt(8) <> IN
+    val i = DFUInt(8) <> IN init(1,2)
     val o = DFUInt(8) <> OUT
     o <> i
   }
 
-  property("DFBlock.codeString") = {
+  trait ContainerConn3 extends DFDesign {
+    val i = DFUInt(8) <> IN
+    val o = DFUInt(8) <> OUT
+    val io1 = new IODesignConn1 {}
+    val io2 = new IODesignConn1 {}
+    i     <> io1.i //Connecting between owner input and child input
+    io1.o <> io2.i //Connecting between siblings (output <> input)
+    io2.o <> o     //Connecting between child output and owner output
+  }
+
+  trait IODesignIf extends DFDesign {
+    val i = DFUInt(8) <> IN
+    val o = DFUInt(8) <> OUT
+    val b = DFBool() <> IN
+    val myIf = ifdf (b) {
+      val myIf2 = ifdf (b) {
+        o := i
+      }.elseifdf(b) {
+        o := i
+      }
+    }.elsedf {
+      o := i
+    }
+    val bb = DFBool()
+    bb.keep
+  }
+
+  property("DFDesign.codeString") = {
     val topIO = new DFDesign {
       val i = DFUInt(8) <> IN init(1, 2)
       val o = DFUInt(8) <> OUT
@@ -28,6 +55,58 @@ class ConnectTest extends Properties("ConnectTest") {
       """.stripMargin
 
     topIO.codeString =@= compare
+  }
+
+  property("ContainerConn3.codeString") = {
+    val top_containerConn3 = new ContainerConn3 {}
+
+    val compare =
+      """
+        |val top_containerConn3 = new DFDesign {
+        |  val i = DFUInt(8) <> IN
+        |  val o = DFUInt(8) <> OUT
+        |  val io1 = new DFDesign {
+        |    val i = DFUInt(8) <> IN init(1, 2)
+        |    val o = DFUInt(8) <> OUT
+        |    o <> i
+        |  }
+        |  val io2 = new DFDesign {
+        |    val i = DFUInt(8) <> IN init(1, 2)
+        |    val o = DFUInt(8) <> OUT
+        |    o <> i
+        |  }
+        |  io1.i <> i
+        |  io2.i <> io1.o
+        |  o <> io2.o
+        |}
+      """.stripMargin
+
+    top_containerConn3.codeString =@= compare
+  }
+
+  property("IODesignIf.codeString") = {
+    val top_ioDesignIf = new IODesignIf {}
+
+    val compare =
+      """
+        |val top_ioDesignIf = new DFDesign {
+        |  val i = DFUInt(8) <> IN
+        |  val o = DFUInt(8) <> OUT
+        |  val b = DFBool() <> IN
+        |  val myIf = ifdf(b) {
+        |    val myIf2 = ifdf(b) {
+        |      o := i
+        |    }.elseifdf(b) {
+        |      o := i
+        |    }
+        |  }.elsedf {
+        |    o := i
+        |  }
+        |  val bb = DFBool()
+        |}
+      """.stripMargin
+
+    top_ioDesignIf.codeString =@= compare
   }
 
 }
