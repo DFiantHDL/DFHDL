00843ea227d3ab23308c1a2f99343594624d8a0f
Oron Port <soronpo@campus.technion.ac.il>
2018-09-21 02:22:28 +0300

    towards MutableLazyBox

--- src/main/scala/DFiant/internals/LazyBox.scala
+++ src/main/scala/DFiant/internals/LazyBox.scala
@@ -15,3 +15,31 @@ object LazyBox {
   def apply[T](value : => T) : LazyBox[T] = new LazyBox[T](value)
 }
 
+//(args : List[MutableLazyBox[_]], func : List[MutableLazyBox[_]] => T)
+abstract class MutableLazyBox[T] private (path : String) {
+  var value : () => T
+  private var visited : Boolean = false
+  private var locked : Boolean = false
+  private var valueOption : Either[T, String] = Right("Uninitialized")
+  protected def updateValue() : Unit
+  private def clearValue() : Unit = {
+    valueOption = Right("Uninitialized")
+    locked = false
+  }
+  @inline def getOrElse[B >: T](default: => B): B
+//  {
+//      if (visited) None
+//      else {
+//        visited = true
+//        Some(value())
+//      }
+//    valueOption.getOrElse(default)
+//  }
+}
+
+
+object MutableLazyBox {
+
+//  def apply[T](value : => T) : MutableLazyBox[T] = new MutableLazyBox[T](value)
+}
+
