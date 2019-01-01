ce4be3b7fd4d8eef56c3641c00aa7dec47fd7b06
Oron Port <soronpo@campus.technion.ac.il>
2019-01-02 01:33:33 +0200

    course changes

--- src/test/scala/RISCV/DMem.scala
+++ src/test/scala/RISCV/DMem.scala
@@ -6,6 +6,23 @@ import DFiant._
 create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 -module_name dmem_bram
 set_property -dict [list CONFIG.Component_Name {dmem_bram} CONFIG.Use_Byte_Write_Enable {true} CONFIG.Byte_Size {8} CONFIG.Write_Width_A {32} CONFIG.Write_Depth_A {4096} CONFIG.Read_Width_A {32} CONFIG.Enable_A {Always_Enabled} CONFIG.Write_Width_B {32} CONFIG.Read_Width_B {32} CONFIG.Register_PortA_Output_of_Memory_Primitives {false}] [get_ips dmem_bram]
  */
+class DMem_Bram_Sim()(implicit ctx : DFDesign.ContextOf[DMem_Bram_Sim]) extends DFDesign {
+  final val wea   = DFBits(4)  <> IN
+  final val addra = DFBits(12) <> IN
+  final val dina  = DFBits(32) <> IN
+  final val douta = DFBits(32) <> OUT
+
+  final val we : DFBool = wea != b0s
+  private val regs = (0 until 256).map(ri => (ri, DFBits[32]))
+  regs.foreachdf(addra(7, 0)) {
+    case (ri, r) =>
+      douta := r
+      ifdf (we) {
+        r := dina
+      }
+  }
+
+}
 
 class DMem_Bram()(implicit ctx : RTComponent.Context) extends RTComponent {
   final val clka  = Clock()
@@ -23,7 +40,7 @@ class DMem(executeInst : ExecuteInst)(implicit ctx : DFDesign.ContextOf[DMem]) e
   private val dataFromMem = DFBits[32] <> OUT
   private val wrEnToMem   = DFBits(4)
   private val dataToMemBH = DFBits[32] //Data to memory modified for byte and half-word writes
-  val bram = new DMem_Bram()
+  val bram = new DMem_Bram_Sim()
   wrEnToMem := b"0000"
   dataToMemBH := dataToMem
   dataFromMem := bram.douta
@@ -68,9 +85,12 @@ class DMem(executeInst : ExecuteInst)(implicit ctx : DFDesign.ContextOf[DMem]) e
         .casedf(b"1")     {wrEnToMem := b"1100"}
     }
     .casedf(DMemSel.SW)   {wrEnToMem := b"1111"}
+
   bram.addra <> addr(13, 2)
   bram.wea <> wrEnToMem
   bram.dina <> dataToMemBH
+
+//  sim.report(msg"DMem~~~>addr: $addr, dmemSel: $dmemSel, dataToMem: $dataToMem, dataToMemBH: $dataToMemBH, wrEnToMem: $wrEnToMem, dataFromMem: $dataFromMem, bram.douta: ${bram.douta}")
 
   final val inst = {
     import executeInst._
