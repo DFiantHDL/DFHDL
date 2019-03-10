53c94a2d47f1b412d4428ddd5d6f9d0b28746e13
Oron Port <soronpo@campus.technion.ac.il>
2019-03-10 18:25:06 +0200

    sorted try

--- examples/sorted_networks/src/main/scala/SortedNetwork.scala
+++ examples/sorted_networks/src/main/scala/SortedNetwork.scala
@@ -1,16 +1,24 @@
 import DFiant._
-import singleton.ops._
-import singleton.twoface._
 
-trait SortedNetwork[W] extends DFDesign {
+trait SortedNetwork extends DFDesign {
   val size : Int
-  val width : TwoFace.Int[W]
+  val width : Int
   final val ingress = List.tabulate(size)(i => DFSInt(width) <> IN  setName(s"ingress$i"))
   final val egress  = List.tabulate(size)(i => DFSInt(width) <> OUT setName(s"egress$i"))
+  implicit class CaS(tuple : (DFSInt[Int], DFSInt[Int])) {
+    private def maxWidth : Int = math.max(tuple._1.width, tuple._2.width)
+    def sortdf() : (DFSInt[Int], DFSInt[Int]) = {
+      val swap = tuple._1 > tuple._2
+      Tuple2(
+        DFSInt(maxWidth).selectdf(swap)(tuple._2, tuple._1),
+        DFSInt(maxWidth).selectdf(swap)(tuple._1, tuple._2)
+      )
+    }
+  }
 }
 
 //A basic compare-and-swap sort network
-trait CaS[W] extends SortedNetwork[W] {
+trait CaS extends SortedNetwork {
   lazy val size : Int = 2
   ifdf(ingress(0) <= ingress(1)) {
     egress(0) := ingress(0)
@@ -21,10 +29,8 @@ trait CaS[W] extends SortedNetwork[W] {
   }
 }
 object CaS {
-//  def apply[W0, W1](ingress0 : DFSInt[W0], ingress1 : DFSInt[W1])(
-//    implicit
-//    ctx : DFAny.Op.Context,
-//    w : singleton.ops.math.Max[W0, W1]
+//  def apply[W0, W1](ingress0 : DFSInt[W0], ingress1 : DFSInt[W1])(implicit ctx : DFAny.Op.Context)
+//  : ()
 }
 
 object SortedNetwork {
