0af1846c4b4ffa4a9c5006809e24c0470a021941
Oron Port <soronpo@campus.technion.ac.il>
2019-09-13 06:06:46 +0300

    paper

--- examples/first-look/src/main/scala/fpga2019/Box.scala
+++ examples/first-look/src/main/scala/fpga2019/Box.scala
@@ -14,12 +14,32 @@
  *     You should have received a copy of the Lesser GNU General Public License
  *     along with DFiant.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package fpga2019
 import DFiant._
 
-trait Fib extends DFDesign {
-  val out = DFUInt[64] <> OUT
-  val fib = DFUInt[64] init (1, 0)
-  fib := fib.prev + fib.prev(2)
-  out := fib.prev(2)
+trait Box extends DFDesign {
+  val iT = DFSInt[16] <> IN
+  val iB = DFSInt[16] <> IN
+  val oT = DFSInt[16] <> OUT
+  val oB = DFSInt[16] <> OUT
+  iT.prev <> oT
+  oB := iB.prev
 }
+
+trait BoxTop extends DFDesign {
+  val iT = DFSInt[16] <> IN init (5, 7)
+  val iB = DFSInt[16] <> IN init (2, 6)
+  val oT = DFSInt[16] <> OUT
+  val oB = DFSInt[16] <> OUT
+  val boxL = new Box {}
+  val boxR = new Box {}
+  boxL.iT <> iT
+  boxL.iB <> iB
+  boxL.oT <> boxR.iT
+  boxL.oB <> boxR.iB
+  boxR.oT <> oT
+  boxR.oB <> oB
+}
+
+object BoxTopApp extends DFApp.VHDLCompiler[BoxTop]
