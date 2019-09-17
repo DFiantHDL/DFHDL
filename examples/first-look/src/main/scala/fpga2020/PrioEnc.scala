2ad539b5eeaca414bec88089f76c125f686cee54
Oron Port <soronpo@campus.technion.ac.il>
2019-09-17 12:47:18 +0300

    paper

---
+++ examples/first-look/src/main/scala/fpga2020/PrioEnc.scala
@@ -0,0 +1,22 @@
+package fpga2020
+
+import DFiant._
+
+import internals.IntExtras
+import internals.BigIntExtras
+import internals.BitVectorExtras
+trait PrioEnc extends DFDesign {
+  lazy val lines = 128
+  final lazy val width = lines.bitsWidth
+  val wire = DFBits(lines) <> IN
+  val wor = DFBits(width) <> OUT
+
+//  wor := b0s
+//  var bv = BigInt(1)
+//  val myIf = ifdf(wire == 1.toBitVector(128)) {
+//    wor := b0s
+//  }
+//  (0 until 128).foldLeft(myIf)((b, i) => b.elseifdf(wire(i) {wor := i.toBitVector(width)}))
+}
+
+object PrioEncApp extends DFApp.VHDLCompiler[PrioEnc]
