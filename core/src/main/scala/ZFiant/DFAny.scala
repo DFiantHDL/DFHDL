91619c6f1d30cbef73aa2893a7f561c4c584bd7a
Oron Port <soronpo@campus.technion.ac.il>
2019-11-27 23:26:24 +0100

    ZFiant

--- core/src/main/scala/ZFiant/DFAny.scala
+++ core/src/main/scala/ZFiant/DFAny.scala
@@ -41,14 +41,14 @@ object DFAny {
 
   trait Token
 
-  sealed abstract class Constructor[Type0 <: DFType, Var0](val dfType : Type0) extends DFAny {
+  sealed abstract class Constructor[Type0 <: DFType, Var0] extends DFAny {
     type Type = Type0
     type Var = Var0
   }
 
-  case class Const[Type <: DFType](override val dfType : Type)(val token : Type#TToken) extends Constructor[Type, false](dfType)
+  final case class Const[Type <: DFType](dfType : Type)(val token : Type#TToken) extends Constructor[Type, false]
 
-  sealed abstract class Initializable[Type <: DFType, Var](override val dfType : Type)(val externalInit : Seq[Type#TToken]) extends Constructor[Type, Var](dfType)
+  sealed abstract class Initializable[Type <: DFType, Var](dfType : Type)(val externalInit : Seq[Type#TToken]) extends Constructor[Type, Var]
 
   sealed abstract class Port[Type <: DFType, Var](override val dfType : Type)(val dir : Port.Dir, externalInit : Seq[Type#TToken]) extends Initializable[Type, Var](dfType)(externalInit)
   object Port {
@@ -57,21 +57,21 @@ object DFAny {
       case object IN extends Dir
       case object OUT extends Dir
     }
-    case class In[Type <: DFType](override val dfType : Type)(externalInit : Seq[Type#TToken]) extends Port[Type, false](dfType)(Dir.IN, externalInit)
-    case class Out[Type <: DFType](override val dfType : Type)(externalInit : Seq[Type#TToken]) extends Port[Type, true](dfType)(Dir.OUT, externalInit)
+    final case class In[Type <: DFType](override val dfType : Type)(externalInit : Seq[Type#TToken]) extends Port[Type, false](dfType)(Dir.IN, externalInit)
+    final case class Out[Type <: DFType](override val dfType : Type)(externalInit : Seq[Type#TToken]) extends Port[Type, true](dfType)(Dir.OUT, externalInit)
   }
 
   case class NewVar[Type <: DFType](override val dfType : Type)(externalInit : Seq[Type#TToken]) extends Initializable[Type, true](dfType)(externalInit)
 
-  sealed abstract class Alias[Type <: DFType, RefVal <: DFAny, Var](override val dfType : Type)(val refVal : RefVal) extends Constructor[Type, Var](dfType)
+  sealed abstract class Alias[Type <: DFType, RefVal <: DFAny, Var](val dfType : Type)(val refVal : RefVal) extends Constructor[Type, Var]
   object Alias {
-    case class AsIs[Type <: DFType, RefVal <: DFAny](override val dfType : Type)(refVal : RefVal) extends Alias[Type, RefVal, RefVal#Var](dfType)(refVal)
-    case class BitsWL[W, L, RefVal <: DFAny](override val refVal : RefVal)(val relWidth : TwoFace.Int[W], val relBitLow : TwoFace.Int[L]) extends Alias[DFBits[W], RefVal, RefVal#Var](DFBits.dfType(relWidth))(refVal)
-    case class Prev[RefVal <: DFAny](override val refVal : RefVal)(val step : Int) extends Alias[RefVal#Type, RefVal, false](refVal.dfType)(refVal)
+    final case class AsIs[Type <: DFType, RefVal <: DFAny](override val dfType : Type)(refVal : RefVal) extends Alias[Type, RefVal, RefVal#Var](dfType)(refVal)
+    final case class BitsWL[W, L, RefVal <: DFAny](override val refVal : RefVal)(val relWidth : TwoFace.Int[W], val relBitLow : TwoFace.Int[L]) extends Alias[DFBits[W], RefVal, RefVal#Var](DFBits.dfType(relWidth))(refVal)
+    final case class Prev[RefVal <: DFAny](override val refVal : RefVal)(val step : Int) extends Alias[RefVal#Type, RefVal, false](refVal.dfType)(refVal)
   }
 
-  sealed abstract class Func[OutType <: DFType](outType : OutType) extends Constructor[OutType, false](outType)
-  case class Func2[OutType <: DFType, L <: DFAny, R <: DFAny](outType: OutType)(left : L, right : R) extends Func(outType)
+  sealed abstract class Func[Type <: DFType](dfType : Type) extends Constructor[Type, false]
+  final case class Func2[Type <: DFType, L <: DFAny, R <: DFAny](dfType: Type)(left : L, right : R) extends Func(dfType)
 
 
 }
