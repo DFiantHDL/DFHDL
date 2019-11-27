b8a84797c1d40db0a923c31cb0ac26a011cc231c
Oron Port <soronpo@campus.technion.ac.il>
2019-11-27 18:55:07 +0100

    ZFiant

--- core/src/main/scala/ZFiant/DFBits.scala
+++ core/src/main/scala/ZFiant/DFBits.scala
@@ -3,14 +3,14 @@ package ZFiant
 import singleton.ops._
 import singleton.twoface._
 
-case class DFBits[W] private (width : TwoFace.Int[W]) extends DFAny.Val[DFBits[W], false] {
+case class DFBits[W] private (width : TwoFace.Int[W]) extends DFType {
   type Width = W
   type TToken = DFBits.Token
 }
 
 object DFBits {
-  def mold[W](width : TwoFace.Int[W]) = new DFBits(width)
-  def apply[W](width : TwoFace.Int[W]) = DFAny.NewVar(new DFBits(width))(Seq())
+  def dfType[W](width : TwoFace.Int[W]) = new DFBits(width)
+  def apply[W](width : TwoFace.Int[W]) = DFAny.NewVar(dfType(width))(Seq())
 
   case class Token(value : Int) extends DFAny.Token
 }
