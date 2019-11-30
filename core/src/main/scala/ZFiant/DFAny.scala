41a3299467a52950dc52bf3a16a6f63791378e41
Oron Port <soronpo@campus.technion.ac.il>
2019-11-30 09:42:29 +0200

    ZFiant

--- core/src/main/scala/ZFiant/DFAny.scala
+++ core/src/main/scala/ZFiant/DFAny.scala
@@ -9,65 +9,79 @@ trait DFType {
   val width : TwoFace.Int[Width]
 }
 object DFType {
-  implicit def ev[T <: DFAny](t : T) : t.Type = t.dfType
+  implicit def ev[T <: DFAny](t : T) : t.TType = t.dfType
 }
 
 trait DFAny extends DFAnyMember {
-  type Type <: DFType
-  type Var
-  val dfType : Type
+  type TType <: DFType
+  type TVar
+  val dfType : TType
   final protected val left : this.type = this
-  protected type AsVal = DFAny.Aux[Type, false]
-  protected type AsVar = DFAny.Aux[Type, true]
-  protected type AsType[T <: DFType] = DFAny.Aux[T, Var]
-  final def bits : AsType[DFBits[dfType.Width]] = DFAny.Alias.BitsWL[dfType.Width, 0, this.type](left)(dfType.width, 0)
-  final def as[AT <: DFType](aliasType : AT) : AsType[AT] = DFAny.Alias.AsIs[AT, this.type](aliasType)(left)
-  final def prev : AsVal = DFAny.Alias.Prev[this.type](left)(1)
+  protected type AsVal = DFAny.Aux[TType, false]
+  protected type AsVar = DFAny.Aux[TType, true]
+  protected type AsType[T <: DFType] = DFAny.Aux[T, TVar]
+  final def bits : AsType[DFBits[dfType.Width]] = DFAny.Alias.BitsWL[dfType.Width, 0, this.type](left, dfType.width, 0)
+  final def as[AT <: DFType](aliasType : AT) : AsType[AT] = DFAny.Alias.AsIs[AT, this.type](aliasType, left)
+  final def prev : AsVal = DFAny.Alias.Prev[this.type](left, 1)
 }
 
 object DFAny {
-  protected type Aux[Type0 <: DFType, Var0] = DFAny {
-    type Type = Type0
-    type Var = Var0
+  protected type Aux[Type <: DFType, Var] = DFAny {
+    type TType = Type
+    type TVar = Var
   }
 
   trait `Op:=`[To <: DFAny, From] {
     def apply(left : To, right : From) : Unit = {}
   }
 
-  implicit class VarOps[L <: DFAny](left : L)(implicit isVar : L#Var =:= true) {
+  implicit class VarOps[L <: DFAny](left : L)(implicit isVar : L#TVar =:= true) {
     def := [R](right : R)(implicit op : `Op:=`[L, R]) : Unit = op(left, right)
   }
 
   trait Token
 
-  sealed abstract class Constructor[Type0 <: DFType, Var0] extends DFAny {
-    type Type = Type0
-    type Var = Var0
+  sealed trait Constructor[Type <: DFType, Var] extends DFAny {
+    type TType = Type
+    type TVar = Var
   }
 
-  final case class Const[Type <: DFType](dfType : Type)(val token : Type#TToken) extends Constructor[Type, false]
+  final case class Const[Type <: DFType](dfType : Type, token : Type#TToken) extends Constructor[Type, false]
 
-  sealed abstract class Initializable[Type <: DFType, Var](dfType : Type)(val externalInit : Seq[Type#TToken]) extends Constructor[Type, Var]
+  sealed trait Initializable[Type <: DFType, Var] extends Constructor[Type, Var] {
+    val externalInit : Seq[TType#TToken]
+  }
 
-  sealed abstract class Port[Type <: DFType, Var](override val dfType : Type)(val dir : Port.Dir, externalInit : Seq[Type#TToken]) extends Initializable[Type, Var](dfType)(externalInit)
+  sealed trait Port[Type <: DFType, Var] extends Initializable[Type, Var] {
+    val dir : Port.Dir
+  }
   object Port {
     sealed trait Dir
     object Dir {
       case object IN extends Dir
       case object OUT extends Dir
     }
-    final case class In[Type <: DFType](override val dfType : Type)(externalInit : Seq[Type#TToken]) extends Port[Type, false](dfType)(Dir.IN, externalInit)
-    final case class Out[Type <: DFType](override val dfType : Type)(externalInit : Seq[Type#TToken]) extends Port[Type, true](dfType)(Dir.OUT, externalInit)
+    final case class In[Type <: DFType](dfType : Type, externalInit : Seq[Type#TToken]) extends Port[Type, false] {
+      val dir : Port.Dir = Dir.IN
+    }
+    final case class Out[Type <: DFType](dfType : Type, externalInit : Seq[Type#TToken]) extends Port[Type, true] {
+      val dir : Port.Dir = Dir.OUT
+    }
   }
 
-  case class NewVar[Type <: DFType](override val dfType : Type)(externalInit : Seq[Type#TToken]) extends Initializable[Type, true](dfType)(externalInit)
+  case class NewVar[Type <: DFType](dfType : Type, externalInit : Seq[Type#TToken]) extends Initializable[Type, true]
 
-  sealed abstract class Alias[Type <: DFType, RefVal <: DFAny, Var](val dfType : Type)(val refVal : RefVal) extends Constructor[Type, Var]
+  trait Alias[Type <: DFType, RefVal <: DFAny, Var] extends Constructor[Type, Var] {
+    val refVal : RefVal
+  }
   object Alias {
-    final case class AsIs[Type <: DFType, RefVal <: DFAny](override val dfType : Type)(refVal : RefVal) extends Alias[Type, RefVal, RefVal#Var](dfType)(refVal)
-    final case class BitsWL[W, L, RefVal <: DFAny](override val refVal : RefVal)(val relWidth : TwoFace.Int[W], val relBitLow : TwoFace.Int[L]) extends Alias[DFBits[W], RefVal, RefVal#Var](DFBits.dfType(relWidth))(refVal)
-    final case class Prev[RefVal <: DFAny](override val refVal : RefVal)(val step : Int) extends Alias[RefVal#Type, RefVal, false](refVal.dfType)(refVal)
+    final case class AsIs[Type <: DFType, RefVal <: DFAny](dfType : Type, refVal : RefVal) extends Alias[Type, RefVal, RefVal#TVar]
+    final case class BitsWL[W, L, RefVal <: DFAny](refVal : RefVal, relWidth : TwoFace.Int[W], relBitLow : TwoFace.Int[L]) extends Alias[DFBits[W], RefVal, RefVal#TVar]{
+      val dfType : TType = DFBits.dfType(relWidth)
+    }
+    final case class Prev[RefVal <: DFAny](refVal : RefVal, step : Int) extends Alias[RefVal#TType, RefVal, false] {
+      val dfType : TType = refVal.dfType
+    }
   }
 
   sealed abstract class Func[Type <: DFType](dfType : Type) extends Constructor[Type, false]
@@ -81,6 +95,7 @@ object DFAny {
 object Test {
   val a = DFUInt(8)
   val aa = a.bits.as(DFUInt(8)).bits
+
 //  a := a
 //  implicitly[aa.Var =:= true]
 }
--- core/src/main/scala/ZFiant/DFBits.scala
+++ core/src/main/scala/ZFiant/DFBits.scala
@@ -10,7 +10,7 @@ case class DFBits[W] private (width : TwoFace.Int[W]) extends DFType {
 
 object DFBits {
   def dfType[W](width : TwoFace.Int[W]) = new DFBits(width)
-  def apply[W](width : TwoFace.Int[W]) = DFAny.NewVar(dfType(width))(Seq())
+  def apply[W](width : TwoFace.Int[W]) = DFAny.NewVar(dfType(width), Seq())
 
   case class Token(value : Int) extends DFAny.Token
 }
