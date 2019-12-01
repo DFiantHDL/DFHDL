8723f08c927c1892a9031b950b41896126b66356
Oron Port <soronpo@campus.technion.ac.il>
2019-12-01 16:00:33 +0200

    ZFiant

--- core/src/main/scala/ZFiant/DFAny.scala
+++ core/src/main/scala/ZFiant/DFAny.scala
@@ -2,10 +2,10 @@ package ZFiant
 
 import singleton.ops._
 import singleton.twoface._
-import DFiant.internals.Meta
+import DFiant.internals._
 
 trait DFType {
-  type TToken <: DFAny.Token
+  type TTokenValue
   type Width
   val width : TwoFace.Int[Width]
 }
@@ -18,9 +18,9 @@ trait DFAny extends DFMember {
   type TVar
   val dfType : TType
   final protected val left : this.type = this
-  protected type AsVal = DFAny.Aux[TType, false]
-  protected type AsVar = DFAny.Aux[TType, true]
-  protected type AsType[T <: DFType] = DFAny.Aux[T, TVar]
+  protected type AsVal = DFAny.Val[TType]
+  protected type AsVar = DFAny.Var[TType]
+  protected type AsType[T <: DFType] = DFAny.ValOrVar[T, TVar]
   final def bits(implicit ctx : DFAny.Context) : AsType[DFBits[dfType.Width]] =
     DFAny.Alias.BitsWL[dfType.Width, 0, this.type](left, dfType.width, 0)
   final def as[AT <: DFType](aliasType : AT)(implicit ctx : DFAny.Context) : AsType[AT] =
@@ -30,10 +30,6 @@ trait DFAny extends DFMember {
 
 object DFAny {
   trait Context extends DFMember.Context
-  protected[ZFiant] type Aux[Type <: DFType, Var] = DFAny {
-    type TType = Type
-    type TVar = Var
-  }
 
   trait `Op:=`[To <: DFAny, From] {
     def apply(left : To, right : From) : Unit = {}
@@ -43,7 +39,155 @@ object DFAny {
     def := [R](right : R)(implicit op : `Op:=`[L, R]) : Unit = op(left, right)
   }
 
-  trait Token
+  abstract class Token[Type <: DFType] {
+    type TValue = Type#TTokenValue
+    val dfType : Type
+    val value : TValue
+    val bubbleMask : BitVector
+  }
+  object Token {
+  }
+//  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  // Token
+//  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  trait Token {self =>
+//    type TValue
+//    protected[DFiant] type TToken <: Token
+//    protected[DFiant] type TPattern <: DFAny.Pattern[TPattern]{type TValue = self.TValue}
+//    val value : TValue
+//    //maximum token value width
+//    val width : Int
+//    final lazy val widthOfValue : Int = scala.math.max(valueBits.lengthOfValue, bubbleMask.lengthOfValue).toInt
+//    val valueBits : BitVector
+//    val bubbleMask : BitVector
+//    //leading zero counter
+//    final lazy val lzc : Int = scala.math.min(valueBits.lzc, bubbleMask.lzc).toInt
+//    final def isBubble : Boolean = !(bubbleMask === BitVector.low(width))
+//    def toBubbleToken : Token
+//
+//    final def bit(relBit : Int) : DFBool.Token = {
+//      val outBitsValue = valueBits.bit(relBit)
+//      val outBubbleMask = bubbleMask.bit(relBit)
+//      new DFBool.Token(outBitsValue, outBubbleMask)
+//    }
+//    final def bits : DFBits.Token = new DFBits.Token(width, valueBits, bubbleMask)
+//    final def bits(relBitHigh : Int, relBitLow : Int) : DFBits.Token = {
+//      val outWidth = relBitHigh - relBitLow + 1
+//      val outBitsValue = valueBits.bits(relBitHigh, relBitLow)
+//      val outBubbleMask = bubbleMask.bits(relBitHigh, relBitLow)
+//      new DFBits.Token(outWidth, outBitsValue, outBubbleMask)
+//    }
+//    final def bitsWL(relWidth : Int, relBitLow : Int) : DFBits.Token = bits(relWidth + relBitLow - 1, relBitLow)
+//    final def replaceWL(relWidth : Int, relBitLow : Int, replacement : DFBits.Token)(
+//      implicit fromBits : DFBits.Token => TToken
+//    ) : TToken = {
+//      val leftWidth = width - (relBitLow + relWidth)
+//      val leftBitLow = relBitLow + relWidth
+//      val rightWidth = relBitLow
+//      val rightBitLow = 0
+//      val leftOption : Option[DFBits.Token] = if (leftWidth > 0) Some(bitsWL(leftWidth, leftBitLow)) else None
+//      val rightOption : Option[DFBits.Token] = if (rightWidth > 0) Some(bitsWL(rightWidth, rightBitLow)) else None
+//      fromBits(List(leftOption, Some(replacement), rightOption).flatten.reduce((l, r) => l ## r))
+//    }
+//    final def == (that : this.type) : DFBool.Token = {
+//      if (this.isBubble || that.isBubble) DFBool.Token(Bubble)
+//      else DFBool.Token(this.valueBits == that.valueBits)
+//    }
+//    final def != (that : this.type) : DFBool.Token = {
+//      if (this.isBubble || that.isBubble) DFBool.Token(Bubble)
+//      else DFBool.Token(this.valueBits != that.valueBits)
+//    }
+//    final def patternMatch(that : TPattern) : DFBool.Token = DFBool.Token(that.matches(this.value), this.isBubble)
+//  }
+//
+//  object Token {
+////    trait Resizable extends Token {
+////      def resize(toWidth : Int) : TToken
+////    }
+////    abstract class Of[V, P <: DFAny.Pattern[P]{type TValue = V}](implicit codeStringOf : CodeStringOf[V]) extends Token {
+////      type TValue = V
+////      protected[DFiant] type TPattern = P
+////      final def codeString : String = if (isBubble) "Φ" else value.codeString
+////    }
+////    implicit class TokenSeqInit[T <: Token](tokenSeq : Seq[T]) {
+////      def prevInit(step : Int) : Seq[T] = {
+////        val length = tokenSeq.length
+////        //No init at all, so invoking prev does not change anything (bubble tokens will be used)
+////        if ((length == 0) || (step == 0)) tokenSeq
+////        //The step is larger or equals to the init sequence, so only the last init token remains
+////        else if (length <= step) Seq(tokenSeq.last)
+////        //More tokens are available than the step size, so we drop the first, according to the step count
+////        else tokenSeq.drop(step)
+////      }
+////      //      def tokenAt(step : Int)(implicit bubbleOf : [T]) : T = prevInit(step - 1).headOption.getOrElse(DFBits.Token(t.dfVal.width, Bubble))
+////      def bits : Seq[DFBits.Token] =
+////        tokenSeq.map(t => t.bits)
+////      def bitsWL(relWidth : Int, relBitLow : Int) : Seq[DFBits.Token] =
+////        tokenSeq.map(t => t.bitsWL(relWidth, relBitLow))
+////      def replaceWL(relWidth : Int, relBitLow : Int, replacement : Seq[DFBits.Token])(
+////        implicit fromBits : DFBits.Token => T
+////      ) : Seq[T] = TokenSeq(tokenSeq, replacement)((t, r) => t.replaceWL(relWidth, relBitLow, r)(fromBits.asInstanceOf[DFBits.Token => t.TToken]).asInstanceOf[T])
+////      def codeString : String = tokenSeq.map(t => t.codeString).mkString("(", ", ", ")")
+////      def patternMatch(pattern : T#TPattern) : Seq[DFBool.Token] = TokenSeq(tokenSeq, pattern)((l, r) => l.patternMatch(r.asInstanceOf[l.TPattern]))
+////    }
+////    def patternMatch[T <: Token, P <: Pattern[_]](tokenSeq : Seq[T], pattern : P) : Seq[DFBool.Token] = TokenSeq(tokenSeq, pattern)((l, r) => l.patternMatch(r.asInstanceOf[l.TPattern]))
+//  }
+//
+//  object TokenSeq {
+//    def apply[O <: Token, T1 <: Token, T2 <: Token, T3 <: Token](t1 : Seq[T1], t2 : Seq[T2], t3 : Seq[T3])(op : (T1, T2, T3) => O) : Seq[O] =
+//      if (t1.isEmpty || t2.isEmpty || t3.isEmpty) Seq() else{
+//        val leftSeq = t1
+//        val rightSeq = t2
+//        val leftSeq2 = leftSeq.zipAll(rightSeq, leftSeq.last, rightSeq.last)
+//        val rightSeq2 = t3
+//        leftSeq2.zipAll(rightSeq2, leftSeq2.last, rightSeq2.last).map(t => op(t._1._1, t._1._2, t._2))
+//      }
+//    def apply[O <: Token, L <: Token, R <: Token](leftSeq : Seq[L], rightSeq : Seq[R])(op : (L, R) => O) : Seq[O] =
+//      if (leftSeq.isEmpty || rightSeq.isEmpty) Seq() else
+//        leftSeq.zipAll(rightSeq, leftSeq.last, rightSeq.last).map(t => op(t._1, t._2))
+//    def apply[O <: Token, L <: Token, R](leftSeq : Seq[L], rightConst : R)(op : (L, R) => O) : Seq[O] =
+//      leftSeq.map(t => op(t, rightConst))
+//    def apply[O <: Token, T <: Token](seq : Seq[T])(op : T => O) : Seq[O] =
+//      seq.map(t => op(t))
+//    def apply[O <: Token, T <: Token, L <: Token](seq : Seq[T], list : List[Seq[L]])(op : (T, List[L]) => O) : Seq[O] = ???
+//  }
+//  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Match Pattern
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  sealed trait Pattern[P <: Pattern[P]] {
+    type TValue
+    def matches(value : TValue) : Boolean
+    def overlapsWith(pattern: P) : Boolean
+    def codeString : String
+    override def toString: String = codeString
+  }
+  object Pattern {
+    abstract class OfIntervalSet[T, P <: OfIntervalSet[T, P]](val patternSet : IntervalSet[T])(implicit codeStringOf: CodeStringOf[Interval[T]]) extends Pattern[P] {
+      type TValue = T
+      def matches(value : TValue) : Boolean = patternSet.containsPoint(value)
+      def overlapsWith(pattern: P) : Boolean = patternSet.intersect(pattern.patternSet).nonEmpty
+      def codeString : String = patternSet.map(t => t.codeString).mkString(", ")
+    }
+    abstract class OfSet[T, P <: OfSet[T, P]](val patternSet : Set[T])(implicit codeStringOf: CodeStringOf[T]) extends Pattern[P] {
+      type TValue = T
+      def matches(value : TValue) : Boolean = patternSet.contains(value)
+      def overlapsWith(pattern: P) : Boolean = patternSet.intersect(pattern.patternSet).nonEmpty
+      def codeString : String = patternSet.map(t => t.codeString).mkString(", ")
+    }
+    trait Able[+R] {
+      val right : R
+    }
+
+//    trait Builder[L <: DFAny, Able[+R] <: Pattern.Able[R]] {
+//      def apply[R](left : L, right : Seq[Able[R]]) : L#TPattern
+//    }
+  }
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
   trait ValOrVar[Type <: DFType, Var] extends DFAny {
     type TType = Type
@@ -57,12 +201,12 @@ object DFAny {
     val ctx : DFAny.Context
   }
 
-  final case class Const[Type <: DFType](dfType : Type, token : Type#TToken)(
+  final case class Const[Type <: DFType](dfType : Type, token : Token[Type])(
     implicit val ctx : DFAny.Context
   ) extends Constructor[Type, false]
 
   sealed trait Initializable[Type <: DFType, Var] extends Constructor[Type, Var] {
-    val externalInit : Seq[TType#TToken]
+    val externalInit : Seq[Token[Type]]
   }
 
   sealed trait Port[Type <: DFType, Var] extends Initializable[Type, Var] {
@@ -74,19 +218,19 @@ object DFAny {
       case object IN extends Dir
       case object OUT extends Dir
     }
-    final case class In[Type <: DFType](dfType : Type, externalInit : Seq[Type#TToken])(
+    final case class In[Type <: DFType](dfType : Type, externalInit : Seq[Token[Type]])(
       implicit val ctx : DFAny.Context
     ) extends Port[Type, false] {
       val dir : Port.Dir = Dir.IN
     }
-    final case class Out[Type <: DFType](dfType : Type, externalInit : Seq[Type#TToken])(
+    final case class Out[Type <: DFType](dfType : Type, externalInit : Seq[Token[Type]])(
       implicit val ctx : DFAny.Context
     ) extends Port[Type, true] {
       val dir : Port.Dir = Dir.OUT
     }
   }
 
-  final case class NewVar[Type <: DFType](dfType : Type, externalInit : Seq[Type#TToken])(
+  final case class NewVar[Type <: DFType](dfType : Type, externalInit : Seq[Token[Type]])(
     implicit val ctx : DFAny.Context
   ) extends Initializable[Type, true] {
     def ifdf(cond : ValOrVar[DFBool, _])(block : => ValOrVar[Type, _])(implicit ctx : DFBlock.Context)
