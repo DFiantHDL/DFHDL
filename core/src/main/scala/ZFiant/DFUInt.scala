768c6e185cefd4f705f33b9b59d763f5f1e3abc0
Oron Port <soronpo@campus.technion.ac.il>
2019-12-04 00:19:10 +0200

    ZFiant

--- core/src/main/scala/ZFiant/DFUInt.scala
+++ core/src/main/scala/ZFiant/DFUInt.scala
@@ -4,15 +4,13 @@ import singleton.ops._
 import singleton.twoface._
 import DFiant.internals._
 
-class DFUInt[W] private (val width : TwoFace.Int[W]) extends DFType {
-  type Width = W
-  type TToken = DFUInt.Token[W]
-  override def toString: String = s"DFUInt($width)"
-}
-
 object DFUInt {
-  def dfType[W](width : TwoFace.Int[W]) = new DFUInt(width)
-  def apply[W](width : TwoFace.Int[W])(implicit ctx : DFAny.Context) = DFAny.NewVar(dfType(width), Seq())
+  final case class Type[W](width : TwoFace.Int[W]) extends DFAny.DFType {
+    type Width = W
+    type TToken = Token[W]
+    override def toString: String = s"DFUInt($width)"
+  }
+  def apply[W](width : TwoFace.Int[W])(implicit ctx : DFAny.Context) = DFAny.NewVar(Type(width), Seq())
 
   final case class Token[W](width : TwoFace.Int[W], value : BigInt, bubble : Boolean) extends DFAny.Token.Of[BigInt, W] {
     lazy val valueBits : XBitVector[W] = value.toBitVector(width)
