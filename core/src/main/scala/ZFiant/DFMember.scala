896571f9a10e4cae4f8827130eff4b813917f95a
Oron Port <soronpo@campus.technion.ac.il>
2019-12-06 18:33:57 +0200

    ZFiant

--- core/src/main/scala/ZFiant/DFMember.scala
+++ core/src/main/scala/ZFiant/DFMember.scala
@@ -3,7 +3,7 @@ import DFiant.internals.Meta
 
 trait DFMember {
   val ctx : DFMember.Context
-  lazy val owner : DFBlock = ctx.owner
+  val owner : DFBlock = ctx.owner
 }
 object DFMember {
   trait Context {
