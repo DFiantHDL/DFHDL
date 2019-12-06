b9e57f721a3c10cad660ae93e410f2c2b3c9f0e7
Oron Port <soronpo@campus.technion.ac.il>
2019-12-06 03:05:57 +0200

    ZFiant

--- core/src/main/scala/ZFiant/DFAny.scala
+++ core/src/main/scala/ZFiant/DFAny.scala
@@ -424,27 +424,3 @@ object DFAny {
   }
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
-
-
-
-object Test {
-  trait BB extends DFBlock {
-//    val a = DFUInt(8)
-//    DFUInt(8).ifdf(???) {
-//      a
-//    }.elsedf {
-//      a
-//    }
-    val b = DFBits(8) <> OUT init b1s
-    val b2 = DFBits(8) <> IN
-    val C = DFBits(8) <> IN init (b"11111111", b0s)
-    b := b2
-    val bb = b"1111111"
-    val z = b == b1s
-  }
-//  val aa = a.bits.as(DFUInt(8)).bits
-
-//  a := a
-//  implicitly[aa.Var =:= true]
-}
-
