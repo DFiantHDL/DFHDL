4b6ad1c702fe9c66c9de7af00c5edc6a2479b387
Oron Port <soronpo@campus.technion.ac.il>
2019-12-16 23:36:32 +0200

    DFDesign.Block

--- core/src/test/scala/ZFiant/ZTest.scala
+++ core/src/test/scala/ZFiant/ZTest.scala
@@ -109,7 +109,7 @@ object ZTest extends App {
   }
 
   val top = new BBB {}
-  println(top.__db.immutable.members)
+  println(top.db.members)
 //  println(top.__compiler.getRefTable)
 
 //  println((top, top.owner), (top.a, top.a.owner), (top.b, top.b.owner), (top.c, top.c.owner))
