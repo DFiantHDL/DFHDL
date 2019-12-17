7e92e29e873a6ae33df985d82323e28311d14994
Oron Port <soronpo@campus.technion.ac.il>
2019-12-17 20:21:32 +0200

    ZFiant

--- core/src/main/scala/ZFiant/DFNet.scala
+++ core/src/main/scala/ZFiant/DFNet.scala
@@ -1,23 +1,22 @@
 package ZFiant
 import DFiant.internals._
 
-sealed trait DFNet extends DFMember {
+sealed abstract class DFNet(op : String) extends DFMember {
   val toRef : DFRef[DFAny]
   val fromRef : DFRef[DFAny]
-  override def toString: String = s"${toRef.refCodeString} := ${fromRef.refCodeString}"
+  override def toString: String = s"${toRef.refCodeString} $op ${fromRef.refCodeString}"
 }
 
 object DFNet {
   type Context = DFAny.Context
 
-  final case class Assignment(toRef : DFRef[DFAny], fromRef : DFRef[DFAny], ownerRef: DFRef[DFBlock], meta: Meta) extends DFNet
+  final case class Assignment(toRef : DFRef[DFAny], fromRef : DFRef[DFAny], ownerRef: DFRef[DFBlock], meta: Meta) extends DFNet(":=")
   object Assignment {
     def apply(to: DFAny, from: DFAny)(implicit ctx: Context)
     : Assignment = ctx.db.addMember(Assignment(to, from, ctx.owner, ctx.meta))
   }
 
-  final case class Connection(toRef : DFRef[DFAny], fromRef : DFRef[DFAny], ownerRef: DFRef[DFBlock], meta: Meta) extends DFNet {
-  }
+  final case class Connection(toRef : DFRef[DFAny], fromRef : DFRef[DFAny], ownerRef: DFRef[DFBlock], meta: Meta) extends DFNet("<>")
   object Connection {
     def apply(to: DFAny, from: DFAny)(implicit ctx: Context)
     : Connection = ctx.db.addMember(Connection(to, from, ctx.owner, ctx.meta))
