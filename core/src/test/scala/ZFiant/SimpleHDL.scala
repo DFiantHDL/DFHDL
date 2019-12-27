ab5b9fbe07fd55089e8a335972a317ef29bd3afa
Oron Port <soronpo@campus.technion.ac.il>
2019-12-27 06:31:46 +0200

    ZFiant

--- core/src/test/scala/ZFiant/SimpleHDL.scala
+++ core/src/test/scala/ZFiant/SimpleHDL.scala
@@ -5,13 +5,41 @@ object ModeType extends Enum.Auto {
   val COUNTING_UP, COUNTING_DOWN, HOLD = Entry()
 }
 trait SimpleHDL extends DFDesign {
+  import ModeType._
   final val max = DFBits(32) <> IN
   final val count = DFBits(32) <> OUT
   final val hold_count = DFBits(32) <> IN
 
-  final val counter = DFUInt(32) init 0 keep
-  final val hold_counter = DFUInt(3) init 0
-  final val mode = DFEnum(ModeType) init ModeType.COUNTING_UP keep
+  final val counter = DFUInt(32) init 0
+  final val hold_counter = DFUInt(32) init 0
+  final val mode = DFEnum(ModeType) init COUNTING_UP
+  matchdf(mode)
+    .casedf(COUNTING_UP) {
+      ifdf(counter >= max.uint){
+        counter := max.uint
+        mode := HOLD
+      }.elsedf {
+        counter := counter + 1
+      }
+    }
+    .casedf(COUNTING_DOWN) {
+      ifdf(counter === 0) {
+        counter := counter + 1
+        mode := COUNTING_UP
+      }.elsedf {
+        counter := counter - 1
+      }
+    }
+    .casedf(HOLD) {
+      ifdf (hold_counter === hold_count.uint) {
+        hold_counter := 0
+        mode := COUNTING_DOWN
+      }.elsedf {
+        hold_counter := hold_counter + 1
+      }
+    }
+
+  count := counter.bits
 }
 
 object SimpleHDLApp extends App {
