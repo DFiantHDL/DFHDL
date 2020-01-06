635c691da275dc05990380fee949e91fe544d363
Oron Port <soronpo@campus.technion.ac.il>
2020-01-06 02:45:26 +0200

    ZFiant

--- core/src/main/scala/ZFiant/maxeler/package.scala
+++ core/src/main/scala/ZFiant/maxeler/package.scala
@@ -5,12 +5,10 @@ package object maxeler {
     def maxJNode : MaxJNode = MaxJNode(design.db)
   }
 
-  trait MaxelerOptions {
-    implicit class PortsTagging[P <: DFAny.PortOf[_ <: DFAny.Type]](port : P) {
-      def setMaxelerStreamIOPush(implicit getSet: MemberGetSet) : Unit = port.addCustomTag(MaxelerStreamIOPush)
-      def setMaxelerStreamIOPull(implicit getSet: MemberGetSet) : Unit = port.addCustomTag(MaxelerStreamIOPull)
-      def setMaxelerScalarIO(implicit getSet: MemberGetSet) : Unit = port.addCustomTag(MaxelerScalarIO)
-    }
+  implicit class PortsTagging[P <: DFAny.PortOf[_ <: DFAny.Type]](port : P) {
+    def setMaxelerStreamIOPush(implicit getSet: MemberGetSet) : Unit = port.addCustomTag(MaxelerStreamIOPush)
+    def setMaxelerStreamIOPull(implicit getSet: MemberGetSet) : Unit = port.addCustomTag(MaxelerStreamIOPull)
+    def setMaxelerScalarIO(implicit getSet: MemberGetSet) : Unit = port.addCustomTag(MaxelerScalarIO)
   }
 
 }
