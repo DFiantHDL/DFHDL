dbcb08557a63d2849938344ca9d9affa7338cea9
Oron Port <soronpo@campus.technion.ac.il>
2020-02-12 15:27:04 +0200

    ZFiant

--- core/src/main/scala/ZFiant/compiler/package.scala
+++ core/src/main/scala/ZFiant/compiler/package.scala
@@ -1,14 +1,14 @@
 package ZFiant
 
 package object compiler {
-//  implicit def evCalculator[C : Compilable](c : C) : Calculator[C] = new Calculator[C](c)
-
   implicit def evUtils[D <: DFDesign, S <: shapeless.HList, C](c : C)(implicit conv : C => Compilable[D, S])
   : UtilsOps[D, S] = new UtilsOps[D, S](c)
   implicit def evExplicitPrev[D <: DFDesign, S <: shapeless.HList, C](c : C)(implicit conv : C => Compilable[D, S]) 
   : ExplicitPrevOps[D, S] = new ExplicitPrevOps[D, S](c)
   implicit def evViaPortConnection[D <: DFDesign, S <: shapeless.HList, C](c : C)(implicit conv : C => Compilable[D, S])
   : ViaPortConnectionOps[D, S] = new ViaPortConnectionOps[D, S](c)
-
-  //  implicit def evFlatten[C : Compilable](c : C) : Flatten[C] = new Flatten[C](c)
+  implicit def evFlatten[D <: DFDesign, S <: shapeless.HList, C](c : C)(implicit conv : C => Compilable[D, S])
+  : FlattenOps[D, S] = new FlattenOps[D, S](c)
+  implicit def evCalculator[D <: DFDesign, S <: shapeless.HList, C](c : C)(implicit conv : C => Compilable[D, S])
+  : CalculatorOps[D, S] = new CalculatorOps[D, S](c)
 }
