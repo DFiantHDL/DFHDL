c4e5e850514dcd5f7dd873658bbe04162e322272
Oron Port <soronpo@tx.technion.ac.il>
2020-04-10 15:53:00 +0300

    DFInterface.Owner

--- core/src/main/scala/DFiant/DFInterface.scala
+++ core/src/main/scala/DFiant/DFInterface.scala
@@ -1,5 +1,6 @@
 package DFiant
 
+import DFiant.DFDesign.DB
 import DFiant.internals.Meta
 
 trait DFInterface extends HasTypeName with DFDesign.Implicits {
@@ -11,5 +12,34 @@ trait DFInterface extends HasTypeName with DFDesign.Implicits {
 
 }
 
+object DFInterface {
+  abstract class Only()(implicit ctx : DFAny.Context) extends DFInterface {self =>
+    private[DFiant] final val owner : Owner = Owner(ctx)
+    private[DFiant] final val __db: DFDesign.DB.Mutable = ctx.db
+    final protected implicit val __getset : MemberGetSet = ctx.db.getSet
+    final protected implicit def __anyContext(implicit meta0 : Meta) : DFAny.Context =
+      new DFAny.Context {
+        val meta : Meta = meta0
+        def owner : DFOwner = self.owner
+        val db : DB.Mutable = self.__db
+      }
+  }
 
+  final case class Owner(ownerRef : DFOwner.Ref, tags : DFMember.Tags.Basic) extends DFOwner {
+    type TTags = DFMember.Tags.Basic
+    type TCustomTag = DFMember.CustomTag
+    protected[DFiant] def =~(that : DFMember)(implicit getSet : MemberGetSet) : Boolean = that match {
+      case Owner(_, tags) => this.tags =~ tags
+      case _ => false
+    }
+    def setTags(tagsFunc : DFMember.Tags.Basic => DFMember.Tags.Basic)(
+      implicit getSet : MemberGetSet
+    ) : DFMember = getSet.set(this)(m => m.copy(tags = tagsFunc(m.tags)))
+  }
+  object Owner {
+    def apply(
+      implicit ctx : DFAny.Context
+    ) : Owner = ctx.db.addMember(Owner(ctx.owner, ctx.meta))
+  }
+}
 
