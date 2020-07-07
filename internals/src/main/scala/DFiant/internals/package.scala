adec1921eb4962d7a8d28dc4d78bf620e1e39159
Oron Port <soronpo@tx.technion.ac.il>
2020-07-07 16:59:58 +0300

    time measurement utility

--- internals/src/main/scala/DFiant/internals/package.scala
+++ internals/src/main/scala/DFiant/internals/package.scala
@@ -392,4 +392,13 @@ package object internals {
       accumulator(seq, f, Nil)
     }
   }
+
+  def tmeas[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0)/1000000 + "ms")
+    result
+  }
+
 }
