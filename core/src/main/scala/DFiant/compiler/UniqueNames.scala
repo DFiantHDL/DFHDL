c837f2d27893751cc1d1a85a31d97533aff8062c
Oron Port <soronpo@tx.technion.ac.il>
2020-07-08 15:17:47 +0300

    UniqueNames now handle enumeration names

--- core/src/main/scala/DFiant/compiler/UniqueNames.scala
+++ core/src/main/scala/DFiant/compiler/UniqueNames.scala
@@ -7,18 +7,43 @@ final class UniqueNamesOps[D <: DFDesign, S <: shapeless.HList](c : IRCompilatio
   private val designDB = c.fixAnonymous.db
   import designDB.__getset
   def uniqueNames(reservedNames : Set[String], caseSensitive : Boolean) = {
-    val reservedNamesLC = if (caseSensitive) reservedNames else reservedNames.map(n => n.toLowerCase)
-    val patchList = designDB.designMemberList.flatMap {
-      case (_, members) => members.filterNot(_.isAnonymous).groupBy(m => if (caseSensitive) m.name else m.name.toLowerCase).flatMap {
-        case (name, list) if list.size > 1 || reservedNamesLC.contains(name) => list.zipWithIndex.map{case (member, i) =>
+    def lowerCase(name : String) : String = if (caseSensitive) name else name.toLowerCase
+    def lowerCases(names : Set[String]) : Set[String] = if (caseSensitive) names else names.map(_.toLowerCase)
+    def renamer[T, R](iter : Iterable[T], existingNamesLC : Set[String])
+      (nameAccess : T => String, updateFunc : (T, String) => R)
+    : Iterable[R] = {
+      iter.groupBy(e => lowerCase(nameAccess(e))).flatMap {
+        case (name, list) if list.size > 1 || existingNamesLC.contains(name) => list.zipWithIndex.map{case (renamed, i) =>
           val suffix = s"%0${list.size.toString.length}d".format(i)
-          val updatedName = s"${member.name}_$suffix"
-          member -> Patch.Replace(member.setName(updatedName), Patch.Replace.Config.FullReplacement)
+          val updatedName = s"${nameAccess(renamed)}_$suffix"
+          updateFunc(renamed, updatedName)
         }
         case _ => Nil
       }
     }
-    c.newStage[UniqueNames](designDB.patch(patchList))
+
+    val reservedNamesLC = lowerCases(reservedNames)
+    val globalTagList = renamer(designDB.getGlobalEnumTypes, reservedNamesLC)(
+      _.name,
+      (e, n) => (e, "name") -> EnumType.NameTag(n)
+    )
+    val globalNames : Set[String] =
+      (designDB.getGlobalEnumTypes.map(e => e.name) ++ globalTagList.map(e => e._2.name) ++ reservedNames)
+    val globalNamesLC = lowerCases(globalNames)
+    val patchesAndTags = designDB.designMemberList.map {case (design, members) =>
+      val localTagList = renamer(designDB.getLocalEnumTypes(design), globalNamesLC)(
+        _.name,
+        (e, n) => (e, "name") -> EnumType.NameTag(n)
+      )
+      val patchList = renamer(members.filterNot(_.isAnonymous), globalNamesLC ++ localTagList.map(e => lowerCase(e._2.name)))(
+        _.name,
+        (m, n) => m -> Patch.Replace(m.setName(n), Patch.Replace.Config.FullReplacement)
+      )
+      (patchList, localTagList)
+    }.unzip
+    val patchList = patchesAndTags._1.flatten
+    val tagList = patchesAndTags._2.flatten ++ globalTagList
+    c.newStage[UniqueNames](designDB.patch(patchList).setGlobalTags(tagList))
   }
 }
 
