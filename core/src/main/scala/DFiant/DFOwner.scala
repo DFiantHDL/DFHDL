b10f0d90041761d10a9f966f1a7a385608a2fdb1
soronpo <soronpo@tx.technion.ac.il>
2020-07-29 01:44:21 +0300

    return debugging defs

--- core/src/main/scala/DFiant/DFOwner.scala
+++ core/src/main/scala/DFiant/DFOwner.scala
@@ -25,6 +25,7 @@ trait DFOwner extends DFMember {
       case _ =>
     }
   }
+  def nameAndType : String = s"$name : $typeName"
 }
 
 object DFOwner {
@@ -48,6 +49,7 @@ object DFOwner {
     protected[DFiant] def onEnterContainer() : Unit = {}
     protected[DFiant] def onExitContainer() : Unit = {}
     protected[DFiant] def onCreateContainer() : Unit = {}
+    def nameAndType : String = s"${__ctx.meta.name} : $typeName"
   }
 
   implicit class AbstractExt[T <: DFOwner](t : T) {
