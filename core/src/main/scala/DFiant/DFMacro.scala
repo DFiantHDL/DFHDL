a33cee8c3a20456ec6498fbf56b12db81ec0d879
soronpo <soronpo@tx.technion.ac.il>
2020-08-12 18:02:12 +0300

    @df annotation macro now handles existing implicit arguments

--- core/src/main/scala/DFiant/DFMacro.scala
+++ core/src/main/scala/DFiant/DFMacro.scala
@@ -9,11 +9,12 @@ object DFMacro {
     import c.universe._
     val result = {
       annottees.map(_.tree).toList match {
-        case q"$mods class $tpname[..$tparams] $ctorMods(...$paramss) extends ..$parents { $self => ..$stats }" :: tail =>
+        case q"$mods class $tpname[..$tparams] $ctorMods(...$paramss)(implicit ..$iparams) extends ..$parents { $self => ..$stats }" :: tail =>
           val targs = tparams.map(t => tq"Nothing") //TODO: needs to fix for the general abstract type
-          q"""$mods class $tpname[..$tparams] $ctorMods(...$paramss)(implicit ctx : ContextOf[$tpname[..$targs]]) extends ..$parents {$self => ..$stats
+          val iparamsWithCtx = iparams :+ q"private[this] val ctx : ContextOf[$tpname[..$targs]]"
+          q"""$mods class $tpname[..$tparams] $ctorMods(...$paramss)(implicit ..$iparamsWithCtx) extends ..$parents {$self => ..$stats
           }; ..$tail"""
-        case q"$mods def $tname[..$tparams](...$paramss): $tpt = $expr" :: Nil =>
+        case q"$mods def $tname[..$tparams](...$paramss)(implicit ..$iparams): $tpt = $expr" :: Nil =>
           val cn = c.internal.constantType(Constant(tname.toString()))
           val q"new df(...$dfParams)" = c.prefix.tree
           val withScope = dfParams match {
@@ -21,7 +22,8 @@ object DFMacro {
             case _ => true
           }
           val defdfTree = if (withScope) q"defdf{$expr}" else expr
-          q"$mods def $tname[..$tparams](...$paramss)(implicit ctx : ContextOf[$cn]) : $tpt = $defdfTree"
+          val iparamsWithCtx = iparams :+ q"ctx : ContextOf[$cn]"
+          q"$mods def $tname[..$tparams](...$paramss)(implicit ..$iparamsWithCtx) : $tpt = $defdfTree"
         case _ => c.abort(c.enclosingPosition, "Annotation @df can be used only with classes or definitions")
       }
     }
