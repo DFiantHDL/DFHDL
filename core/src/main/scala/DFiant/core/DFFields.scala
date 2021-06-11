b10510d57816171da9d0bccc0752fe66b1ce1c3b
soronpo <soronpo@tx.technion.ac.il>
2021-06-11 17:11:54 +0300

    DFiant3 wip

---
+++ core/src/main/scala/DFiant/core/DFFields.scala
@@ -0,0 +1,27 @@
+package DFiant
+package core
+import internals.*
+
+import scala.collection.mutable
+
+/////////////////////////////////////////////////////////////////////////////
+// DFFields are used for either struct or enumerations (tagged unions)
+/////////////////////////////////////////////////////////////////////////////
+abstract class DFFields(using meta: MetaContext) extends Product, Serializable:
+  final private val all =
+    mutable.ListBuffer.empty[DFField[_ <: DFType]]
+  final lazy val getFields = all.toList
+  final val name: String = meta.clsNameOpt.get
+  protected sealed trait FIELD
+  protected object FIELD extends FIELD
+  extension [T <: DFType.Supported](t: T)(using tc: DFType.TC[T])
+    def <>(FIELD: FIELD)(using MetaContext): DFField[tc.Type] =
+      val dfType = tc(t)
+      val field = DFField(dfType)
+      all += field
+      field
+final case class DFField[Type <: DFType](dfType: Type)(using
+    meta: MetaContext
+):
+  val name: String = meta.name
+/////////////////////////////////////////////////////////////////////////////
