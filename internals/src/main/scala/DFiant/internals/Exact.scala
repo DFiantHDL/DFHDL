fe254015ee148bbef91ea559b02d13f58ddbc0b7
soronpo <soronpo@tx.technion.ac.il>
2021-06-29 01:02:07 +0300

    DFiant3 wip

--- internals/src/main/scala/DFiant/internals/Exact.scala
+++ internals/src/main/scala/DFiant/internals/Exact.scala
@@ -1,27 +1,37 @@
 package DFiant.internals
-
-//class Exact[T](val value: T)
+import scala.quoted.*
 opaque type Exact[T] = T
 object Exact:
-//  import scala.reflect.macros.blackbox
-//  //For singleton integers we create a special macro that offers some protection from hex literals that
-//  //overflow into negative values. E.g., 0x80000000
-//  //This is no way close to a full protection from such incidents, but this is enough for most newbie cases
-//  //that DFiant code may encounter.
-//  implicit def fromIntSing[T <: Int with Singleton](value : T) : Exact[ValueOf[T]] = macro fromIntSingMacro[T]
-//  def fromIntSingMacro[T : c.WeakTypeTag](c: blackbox.Context)(value : c.Tree) : c.Tree = {
-//    import c.universe._
-//    val tpe = weakTypeOf[T]
-//    value match {
-//      case Literal(Constant(i : Int)) if i < 0 =>
-//        val content = new String(value.pos.source.content)
-//        val startStr = content.splitAt(value.pos.start)._2
-//        if (startStr.startsWith("0x") || startStr.startsWith("0X"))
-//          c.abort(c.enclosingPosition, "Found a hex integer literal that overflows into a negative value. \nPlease use DFiant's built in string interpolator literals instead.")
-//      case _ => //do nothing
-//    }
-//    q"new DFiant.internals.Exact[ValueOf[$tpe]](new ValueOf($value))"
-//  }
+  //For singleton integers we create a special macro that offers some protection from hex literals that
+  //overflow into negative values. E.g., 0x80000000
+  //This is no way close to a full protection from such incidents, but this is enough for most newbie cases
+  //that DFiant code may encounter.
+  implicit inline def fromIntSing[T <: Int with Singleton](
+      inline value: T
+  ): Exact[ValueOf[T]] = fromIntSing2(value)
+  //TODO: remove when https://github.com/lampepfl/dotty/issues/12975 is resolved
+  inline def fromIntSing2[T <: Int](
+      inline value: T
+  ): Exact[ValueOf[T]] = ${ fromIntSingMacro[T]('value) }
+  def fromIntSingMacro[T <: Int](
+      value: Expr[T]
+  )(using Quotes, Type[T]): Expr[Exact[ValueOf[T]]] = {
+    import quotes.reflect.*
+    val valueTerm = value.asTerm.underlyingArgument
+    valueTerm match
+      case Literal(IntConstant(i: Int)) if i < 0 =>
+        val pos = Position.ofMacroExpansion
+        val content = pos.sourceCode.get
+        if (content.startsWith("0x") || content.startsWith("0X"))
+          val properText = s"""h"${content.drop(2)}""""
+          report.error(
+            s"""Found a hex integer literal that overflows into a negative value.
+               |Please use DFiant's built in string interpolator literals instead.
+               |E.g.: $properText""".stripMargin
+          )
+      case _ => //do nothing
+    '{ ValueOf[T]($value) }
+  }
   implicit def fromAnyValSing[T <: Singleton with AnyVal](
       value: T
   ): Exact[ValueOf[T]] = ValueOf(value)
