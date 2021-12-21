5c0cbb53718ad042eee416e43055c240d7c17a64
oronpo <oron.port@cornell.edu>
2021-12-20 23:08:56 -0500

    DFiant wip

--- core/src/test/scala/DFiant/DFStructSpec.scala
+++ core/src/test/scala/DFiant/DFStructSpec.scala
@@ -3,9 +3,13 @@ import DFiant.core.SameBitsVector
 import munit.*
 
 class DFStructSpec extends DFSpec:
-  case class XY(x: DFBits[8] <> VAL, y: DFUInt[8] <> VAL)
-  case class XYZ(x: DFUInt[8] <> VAL, y: DFBits[8] <> VAL, z: DFBit <> VAL)
-//  def test(t: core.DFValOf[core.DFStruct[Oron]]): Unit =
+  class CCs[W <: Int]:
+    case class XY(x: DFBits[W] <> VAL, y: DFUInt[W] <> VAL)
+    case class XYZ(x: DFUInt[W] <> VAL, y: DFBits[W] <> VAL, z: DFBit <> VAL)
+
+  val cc = new CCs[8]
+  import cc.{XY, XYZ}
+  //  def test(t: core.DFValOf[core.DFStruct[Oron]]): Unit =
 //    t match
 //      case Oron(Zeros, y) if y == 22 =>
 //      case o: Oron                   =>
