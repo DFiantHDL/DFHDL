806b00df8bfa204a29b176dc8394e69d687227e7
oronpo <oron.port@cornell.edu>
2022-02-02 10:28:24 -0500

    DFiant wip

--- compiler/stages/src/test/scala/StagesSpec/ExplicitNamedVarsSpec.scala
+++ compiler/stages/src/test/scala/StagesSpec/ExplicitNamedVarsSpec.scala
@@ -32,7 +32,11 @@ class ExplicitNamedVarsSpec extends StageSpec:
         if (x > 0) 5
         else if (x < 0) x + 1
         else x
-      y := z
+      val z2: DFSInt[16] <> VAL =
+        z match
+          case 1 | 2 => 17
+          case _     => z + 12
+      y := z2
     val id = (new ID).explicitNamedVars
     assertCodeString(
       id,
@@ -43,10 +47,48 @@ class ExplicitNamedVarsSpec extends StageSpec:
          |  if (x > d"16'0") z := sd"16'5"
          |  else if (x < d"16'0") z := x + sd"2'1"
          |  else z := x
-         |  y := z
+         |  val z2 = DFSInt(16) <> VAR
+         |  z match
+         |    case sd"16'1" | sd"16'2" => z2 := sd"16'17"
+         |    case _ => z2 := z + sd"5'12"
+         |  y := z2
          |end ID
          |""".stripMargin
     )
   }
+//  test("Nested named conditional expression") {
+//    class ID(using DFC) extends DFDesign:
+//      val x = DFSInt(16) <> IN
+//      val y = DFSInt(16) <> OUT
+//      val z: DFSInt[16] <> VAL =
+//        if (x > 0)
+//          if (x > 5) 5
+//          else -5
+//        else if (x < 0) x + 1
+//        else x
+////      val z2: DFSInt[16] <> VAL =
+////        z match
+////          case 1 | 2 => 17
+////          case _     => z + 12
+//      y := z
+//    val id = (new ID).explicitNamedVars
+//    assertCodeString(
+//      id,
+//      """|class ID(using DFC) extends DFDesign:
+//         |  val x = DFSInt(16) <> IN
+//         |  val y = DFSInt(16) <> OUT
+//         |  val z = DFSInt(16) <> VAR
+//         |  if (x > d"16'0") z := sd"16'5"
+//         |  else if (x < d"16'0") z := x + sd"2'1"
+//         |  else z := x
+//         |  val z2 = DFSInt(16) <> VAR
+//         |  z match
+//         |    case sd"16'1" | sd"16'2" => z2 := sd"16'17"
+//         |    case _ => z2 := z + sd"5'12"
+//         |  y := z2
+//         |end ID
+//         |""".stripMargin
+//    )
+//  }
 
 end ExplicitNamedVarsSpec
