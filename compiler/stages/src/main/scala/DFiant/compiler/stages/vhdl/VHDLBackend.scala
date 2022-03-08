83fb077d7b9ceb85a184b79b4feb771cb2e06844
oronpo <oron.port@cornell.edu>
2022-03-08 10:16:11 -0500

    DFiant wip

--- compiler/stages/src/main/scala/DFiant/compiler/stages/vhdl/VHDLBackend.scala
+++ compiler/stages/src/main/scala/DFiant/compiler/stages/vhdl/VHDLBackend.scala
@@ -5,17 +5,34 @@ import DFiant.compiler.analysis.*
 import DFiant.compiler.ir.*
 import DFiant.compiler.printing.*
 
+private val reservedKeywords: Set[String] = Set(
+  "abs", "access", "after", "alias", "all", "and", "architecture", "array", "assert", "attribute",
+  "begin", "block", "body", "buffer", "bus", "case", "component", "configuration", "constant",
+  "disconnect", "downto", "else", "elsif", "end", "entity", "exit", "file", "for", "function",
+  "generate", "generic", "group", "guarded", "if", "impure", "in", "inertial", "inout", "is",
+  "label", "library", "linkage", "literal", "loop", "map", "mod", "nand", "new", "next", "nor",
+  "not", "null", "of", "on", "open", "or", "others", "out", "package", "port", "postponed",
+  "procedure", "process", "pure", "range", "record", "register", "reject", "rem", "report",
+  "return", "rol", "ror", "select", "severity", "signal", "shared", "sla", "sll", "sra", "srl",
+  "subtype", "then", "to", "transport", "type", "unaffected", "units", "until", "use", "variable",
+  "wait", "when", "while", "with", "xnor", "xor"
+)
+
 private class VHDLBackend(db: DB) extends Stage(db):
   override protected def preTransform: DB =
-    val updatedDB = db.dropUnreferenced.uniqueDesigns
+    val updatedDB = db /*.dropUnreferenced*/ .uniqueDesigns
     val designNames = updatedDB.members.collect { case block: DFDesignBlock => block.dclName }
-    updatedDB.uniqueNames(designNames.toSet, caseSensitive = true) // .toLLRT
+    updatedDB
+      .uniqueNames(designNames.toSet ++ reservedKeywords, caseSensitive = false)
+      .viaConnection
+      .toLLRT
   override def transform: DB =
     println(toVHDL)
     designDB
   def toVHDL: String =
     given Printer = new RTPrinter
     designDB.codeString
+end VHDLBackend
 
 extension [T: HasDB](t: T)
   def printVHDLCode: DB = new VHDLBackend(t.db).transform
--- compiler/stages/src/test/scala/StagesSpec/PrintVHDLCodeSpec.scala
+++ compiler/stages/src/test/scala/StagesSpec/PrintVHDLCodeSpec.scala
@@ -1,7 +1,7 @@
 package StagesSpec
 
 import DFiant.*
-import DFiant.compiler.stages.vhdl.printVHDLCode
+import DFiant.compiler.stages.vhdl.{printVHDLCode, getVHDLCode}
 // scalafmt: { align.tokens = [{code = "<>"}, {code = "="}, {code = "=>"}, {code = ":="}]}
 
 class PrintVHDLCodeSpec extends StageSpec:
@@ -11,15 +11,26 @@ class PrintVHDLCodeSpec extends StageSpec:
     y := x
 
   test("Basic ID design") {
-    val id = (new ID).printVHDLCode
-//    assertNoDiff(
-//      id,
-//      """|class ID(using DFC) extends DFDesign:
-//         |  val x = DFSInt(16) <> IN
-//         |  val y = DFSInt(16) <> OUT
-//         |  y := x
-//         |end ID
-//         |""".stripMargin
-//    )
+    val id = (new ID).getVHDLCode
+    assertNoDiff(
+      id,
+      """|library ieee;
+         |use ieee.std_logic_1164.all;
+         |use ieee.numeric_std.all;
+         |use work.ID_pkg.all;
+         |
+         |entity ID is
+         |port (
+         |  x : in signed(15 downto 0);
+         |  y : out signed(15 downto 0)
+         |);
+         |end ID;
+         |
+         |architecture ID_arch of ID is
+         |begin
+         |  y := x
+         |end ID_arch;
+         |""".stripMargin
+    )
   }
 end PrintVHDLCodeSpec
