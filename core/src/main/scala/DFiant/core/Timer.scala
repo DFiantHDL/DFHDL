4b65ad2bb37209ca59546183ee6e30e3a8a35616
oronpo <oron.port@cornell.edu>
2022-03-27 20:35:44 -0400

    DFiant wip

--- core/src/main/scala/DFiant/core/Timer.scala
+++ core/src/main/scala/DFiant/core/Timer.scala
@@ -1,31 +1,42 @@
 package DFiant.core
-
+import DFiant.internals.*
 class Time(value: BigDecimal)
-class Timer(period: Time):
-  def *(arg: Int): Timer = ???
-  def *(arg: Double): Timer = ???
-  def /(arg: Int): Timer = ???
-  def /(arg: Double): Timer = ???
+class Freq(value: BigDecimal)
+sealed trait Timer:
+  def *[T <: Int | Double](arg: Inlined[T]): Timer = ???
+  def /[T <: Int | Double](arg: Inlined[T]): Timer = ???
+  def isActive: DFValOf[DFBool] = ???
   def delay(arg: Time): Timer = ???
+
 object Timer:
+  def apply(freq: Freq): Timer = ???
+  def apply(period: Time): Timer = ???
+  final case class Ratio()
+  final class Periodic(period: Time) extends Timer
+  final class DerivedDelay(origin: Timer, delay: Time) extends Timer
+  final class DerivedRatio(origin: Timer, ratio: Ratio) extends Timer
+  final class FromDFBit(bit: DFValOf[DFBit]) extends Timer
+  final class Unspecified() extends Timer
+  extension (bit: DFValOf[DFBit]) def asTimer: Timer = ???
+  trait Literal[T]:
+    def apply(arg: T): BigDecimal
+  object Literal:
+    given fromInt[T <: Int]: Literal[T] with
+      def apply(arg: T): BigDecimal = BigDecimal(arg)
+    given fromDouble[T <: Double]: Literal[T] with
+      def apply(arg: T): BigDecimal = BigDecimal(arg)
   object Ops:
-    extension (lhs: Int)
+    extension (lhs: Int | Double)
       def ps: Time = ???
       def ns: Time = ???
       def us: Time = ???
       def ms: Time = ???
       def units: Time = ???
-    extension (lhs: Double)
-      def ps: Time = ???
-      def ns: Time = ???
-      def us: Time = ???
-      def ms: Time = ???
-      def units: Time = ???
-    extension (lhs: Int)
-      def Hz: Timer = ???
-      def KHz: Timer = ???
-      def MHz: Timer = ???
-      def GHz: Timer = ???
+    extension (lhs: Int | Double)
+      def Hz: Freq = ???
+      def KHz: Freq = ???
+      def MHz: Freq = ???
+      def GHz: Freq = ???
 
   end Ops
 end Timer
