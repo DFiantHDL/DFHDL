8b02d2cf4e8074f94284f44024d26c3c6dcb2438
oronpo <oron.port@cornell.edu>
2022-04-04 08:35:45 -0400

    DFiant wip

--- core/src/main/scala/DFiant/core/Container.scala
+++ core/src/main/scala/DFiant/core/Container.scala
@@ -3,6 +3,8 @@ import DFiant.internals.*
 import DFiant.compiler.ir
 
 private abstract class Container(using DFC) extends OnCreateEvents, HasDFC:
+  // TODO: revisit this. Maybe has something to do with errors and preventing repeated
+  // messages. Need to check and document properly or simplify to `= summon[DFC]`
   final val dfc: DFC =
     val ret = summon[DFC]
     this match
