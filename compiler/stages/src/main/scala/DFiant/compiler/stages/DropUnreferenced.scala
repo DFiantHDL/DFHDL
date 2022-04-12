41bb289d496a16f791bca7f554160b3e87719b3d
oronpo <oron.port@cornell.edu>
2022-04-12 12:17:44 -0400

    DFiant wip

--- compiler/stages/src/main/scala/DFiant/compiler/stages/DropUnreferenced.scala
+++ compiler/stages/src/main/scala/DFiant/compiler/stages/DropUnreferenced.scala
@@ -3,12 +3,14 @@ import DFiant.compiler.ir.*
 import DFiant.compiler.patching.*
 import DFiant.compiler.analysis.*
 
-private class DropUnreferenced(db: DB) extends Stage(db):
-  override def transform: DB =
+case object DropUnreferenced extends Stage2:
+  def dependencies: List[Stage2] = List()
+  def nullifies: Set[Stage2] = Set()
+  def transform(designDB: DB)(using MemberGetSet): DB =
     val patchList = designDB.members.collect {
       case m @ DclVar() if !designDB.memberTable.contains(m) && m.externalInit.isEmpty =>
         m -> Patch.Remove
     }
     designDB.patch(patchList)
 
-extension [T: HasDB](t: T) def dropUnreferenced: DB = new DropUnreferenced(t.db).transform
+extension [T: HasDB](t: T) def dropUnreferenced: DB = StageRunner.run(DropUnreferenced)(t.db)
