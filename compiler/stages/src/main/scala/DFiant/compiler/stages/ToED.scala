a4e382f81fd9902d3c669abe1990a7f8ef7cea31
oronpo <oron.port@cornell.edu>
2022-04-12 11:59:27 -0400

    DFiant wip

--- compiler/stages/src/main/scala/DFiant/compiler/stages/ToED.scala
+++ compiler/stages/src/main/scala/DFiant/compiler/stages/ToED.scala
@@ -4,13 +4,14 @@ import DFiant.compiler.analysis.*
 import DFiant.compiler.ir.*
 import DFiant.compiler.patching.*
 
-private class ToED(db: DB) extends Stage(db):
-  override protected def preTransform: DB = db.toRT
-  override def transform: DB = db
+case object ToED extends Stage2:
+  def dependencies: List[Stage2] = List(ToRT)
+  def nullifies: Set[Stage2] = Set()
+  def transform(designDB: DB)(using MemberGetSet): DB = designDB
 //    val patchList = designDB.members.collect {
 //      case m @ DclVar() if !designDB.memberTable.contains(m) && m.externalInit.isEmpty =>
 //        m -> Patch.Remove
 //    }
 //    designDB.patch(patchList)
 
-extension [T: HasDB](t: T) def toED: DB = new ToED(t.db).transform
+extension [T: HasDB](t: T) def toED: DB = StageRunner.run(ToED)(t.db)
