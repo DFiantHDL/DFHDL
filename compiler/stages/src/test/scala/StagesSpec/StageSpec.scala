926883dfcc7f3f77f7b0573dd38bdde5b7c2ae6d
oronpo <oron.port@cornell.edu>
2022-05-14 20:45:46 -0400

    DFiant wip

--- compiler/stages/src/test/scala/StagesSpec/StageSpec.scala
+++ compiler/stages/src/test/scala/StagesSpec/StageSpec.scala
@@ -5,7 +5,7 @@ import dfhdl.compiler.printing.{DefaultPrinter, Printer, codeString}
 import dfhdl.internals.AllowTopLevel
 import munit.*
 
-class StageSpec extends FunSuite, AllowTopLevel:
+abstract class StageSpec extends FunSuite, AllowTopLevel:
   def assertCodeString(db: DB, cs: String): Unit =
     import db.getSet
     given Printer = DefaultPrinter
