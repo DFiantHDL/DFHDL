0874a8c4db35ca8184993a22ff00f9eb8e355f78
oronpo <oron.port@cornell.edu>
2023-01-27 09:45:50 -0500

    move HasNamePosWithVars for test only

--- core/src/test/scala/CoreSpec/PluginSpec.scala
+++ core/src/test/scala/CoreSpec/PluginSpec.scala
@@ -96,6 +96,15 @@ class PluginSpec extends DFSpec:
   }
   assertLastNames("wrappedTryName")
 
+  trait HasNamePosWithVars extends HasNamePos:
+    private var _clsName: String = ""
+    private var _clsPosition: Position = Position.unknown
+    final protected def setClsNamePos(name: String, position: Position): Unit =
+      _clsName = name
+      _clsPosition = position
+    final def clsName: String = _clsName
+    final def clsPosition: Position = _clsPosition
+
   class GotName extends HasNamePosWithVars
   val gotName = new GotName
   assertEquals(gotName.clsName, "GotName")
--- internals/src/main/scala/dfhdl/internals/HasNamePos.scala
+++ internals/src/main/scala/dfhdl/internals/HasNamePos.scala
@@ -3,11 +3,3 @@ package dfhdl.internals
 trait HasNamePos:
   protected def setClsNamePos(name: String, position: Position): Unit
 
-trait HasNamePosWithVars extends HasNamePos:
-  private var _clsName: String = ""
-  private var _clsPosition: Position = Position.unknown
-  final protected def setClsNamePos(name: String, position: Position): Unit =
-    _clsName = name
-    _clsPosition = position
-  final def clsName: String = _clsName
-  final def clsPosition: Position = _clsPosition
