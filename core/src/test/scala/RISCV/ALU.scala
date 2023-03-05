0777422c51dd52e63c0487de2b3804003e21e741
oronpo <oron.port@cornell.edu>
2023-03-05 11:49:52 -0500

    more flexible types

--- core/src/test/scala/RISCV/ALU.scala
+++ core/src/test/scala/RISCV/ALU.scala
@@ -8,26 +8,21 @@ class ALU extends DFDesign:
   val aluSel = ALUSel   <> IN
   val aluOut = Bits(32) <> OUT
 
-  // helper casted values
-  private val op1u  = op1.uint
-  private val op2u  = op2.uint
-  private val op1s  = op1.sint
-  private val op2s  = op2.sint
   private val shamt = op2(4, 0)
 
   import ALUSel.*
-  val outCalc: Bits[32] <> VAL = aluSel match
-    case ADD   => (op1u + op2u).bits
-    case SUB   => (op1u - op2u).bits
+  private val outCalc: Bits[32] <> VAL = aluSel match
+    case ADD   => op1 + op2
+    case SUB   => op1 - op2
     case AND   => op1 & op2
     case OR    => op1 | op2
     case XOR   => op1 ^ op2
-    case SLT   => (op1s < op2s).bits.resize(32)
-    case SLTU  => (op1u < op2u).bits.resize(32)
+    case SLT   => (op1.sint < op2.sint).bits.resize(32)
+    case SLTU  => (op1 < op2).bits.resize(32)
     case SLL   => op1 << shamt
     case SRL   => op1 >> shamt
-    case SRA   => (op1s >> shamt).bits
+    case SRA   => (op1.sint >> shamt).bits
     case COPY1 => op1
-
+    case _     => ?
   aluOut := outCalc
 end ALU
