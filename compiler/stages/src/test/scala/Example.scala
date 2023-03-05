718bc33947bbc080f0c2b86fb05d8b18f178f66d
oronpo <oron.port@cornell.edu>
2023-03-05 11:18:12 -0500

    more flexible types

--- compiler/stages/src/test/scala/Example.scala
+++ compiler/stages/src/test/scala/Example.scala
@@ -12,25 +12,20 @@ class ALU extends EDDesign:
   val aluOut = Bits(32) <> OUT
 
   process(all) {
-    // helper casted values
-    val op1u  = op1.uint
-    val op2u  = op2.uint
-    val op1s  = op1.sint
-    val op2s  = op2.sint
     val shamt = op2(4, 0)
 
     import ALUSel.*
     val outCalc: Bits[32] <> VAL = aluSel match
-      case ADD   => (op1u + op2u).bits
-      case SUB   => (op1u - op2u).bits
+      case ADD   => op1 + op2
+      case SUB   => op1 - op2
       case AND   => op1 & op2
       case OR    => op1 | op2
       case XOR   => op1 ^ op2
-      case SLT   => (op1s < op2s).bits.resize(32)
-      case SLTU  => (op1u < op2u).bits.resize(32)
+      case SLT   => (op1.sint < op2.sint).bits.resize(32)
+      case SLTU  => (op1 < op2).bits.resize(32)
       case SLL   => op1 << shamt
       case SRL   => op1 >> shamt
-      case SRA   => (op1s >> shamt).bits
+      case SRA   => (op1.sint >> shamt).bits
       case COPY1 => op1
       case _     => ?
     aluOut := outCalc
