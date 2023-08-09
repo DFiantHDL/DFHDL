3e97284a8c82b9472a99d5025fdebfc30904eebd
oronpo <oron.port@cornell.edu>
2023-08-09 14:48:37 +0300

    wip

--- compiler/ir/src/main/scala/dfhdl/options/PrinterOptions.scala
+++ compiler/ir/src/main/scala/dfhdl/options/PrinterOptions.scala
@@ -6,13 +6,21 @@ final case class PrinterOptions(
     color: Color
 )
 object PrinterOptions:
-  given default(using align: Align = true, color: Color = true): PrinterOptions = PrinterOptions(
-    align = align,
-    color = color
-  )
+
+  // detecting if running in Scastie by checking the PWD
+  private def inScastie: Boolean =
+    System.getProperty("user.dir").startsWith("/tmp/scastie")
+
+  // disabling color if in Scastie because of https://github.com/scalacenter/scastie/issues/492
+  given default(using align: Align = true, color: Color = !inScastie): PrinterOptions =
+    PrinterOptions(
+      align = align,
+      color = color
+    )
 
   opaque type Align <: Boolean = Boolean
   given Conversion[Boolean, Align] = x => x
 
   opaque type Color <: Boolean = Boolean
   given Conversion[Boolean, Color] = x => x
+end PrinterOptions
