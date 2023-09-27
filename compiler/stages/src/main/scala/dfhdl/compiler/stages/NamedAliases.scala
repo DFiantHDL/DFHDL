27cceb5f38bb2a6d5cbb0d19bd5d5e39dd42d584
oronpo <oron.port@cornell.edu>
2023-09-28 00:03:08 +0300

    named common aliases

--- compiler/stages/src/main/scala/dfhdl/compiler/stages/NamedAliases.scala
+++ compiler/stages/src/main/scala/dfhdl/compiler/stages/NamedAliases.scala
@@ -3,6 +3,7 @@ package dfhdl.compiler.stages
 import dfhdl.compiler.analysis.*
 import dfhdl.compiler.ir.*
 import dfhdl.compiler.patching.*
+import dfhdl.internals.*
 import DFVal.Func.Op as FuncOp
 
 // Names an anonymous relative value which is aliased.
@@ -13,23 +14,47 @@ private abstract class NamedAliases(criteria: NamedAliases.Criteria) extends Sta
 
   def transform(designDB: DB)(using MemberGetSet): DB =
 
-    val patchList =
+    val patchList: List[(DFMember, Patch)] =
       designDB.members.view
+        // just values
         .collect { case dfVal: DFVal => dfVal }
-        .flatMap { dfVal => criteria()(dfVal) }.filter {
+        // get all that meet the criteria
+        .flatMap { dfVal => criteria()(dfVal) }
+        // filter out the trivial cases (no need to name)
+        .filter {
           case _: DFVal.Const => false // ignore constants
           case _: DFVal.Alias.History =>
             false // history values will get proper names in another stage
           case _: DFVal.Alias.ApplyIdx    => false // ignore index selection
           case _: DFVal.Alias.SelectField => false // ignore field selection
           case _                          => true
-        }.map { m =>
-          // we force set the underlying original name before it was anonymized
-          val namedMember = m.setName(m.suggestName.getOrElse("anon"))
-          m -> Patch.Replace(
-            namedMember,
-            Patch.Replace.Config.FullReplacement
-          )
+        }
+        // tuple with the suggested name
+        .map(m => (m, m.suggestName.getOrElse("anon")))
+        // group dfhdl-equivalent values, as long as they are in the same scope
+        .groupByCompare(
+          (l, r) => l._1 =~ r._1 && l._1.isInsideOwner(r._1.getOwner),
+          _._2.hashCode()
+        )
+        // split to list of aliases and list of suggested names for each group
+        .map(_.unzip)
+        // for each group use just the head to create the named member and patch
+        // all members to reference that member
+        .flatMap {
+          case (firstAlias :: restOfAliases, suggestedName :: _) =>
+            // we force set the underlying original name before it was anonymized
+            val namedMember = firstAlias.setName(suggestedName)
+            // first alias gets a full replacement
+            val firstPatch =
+              firstAlias -> Patch.Replace(namedMember, Patch.Replace.Config.FullReplacement)
+            // the rest of the aliases (if there are any) are just a reference change
+            firstPatch :: restOfAliases.map(
+              _ -> Patch.Replace(
+                namedMember,
+                Patch.Replace.Config.ChangeRefOnly
+              )
+            )
+          case _ => Nil
         }.toList
     designDB.patch(patchList)
   end transform
---
+++ compiler/stages/src/test/scala/StagesSpec/NamedSelectionSpec.scala
@@ -0,0 +1,42 @@
+package StagesSpec
+
+import dfhdl.*
+import dfhdl.compiler.stages.namedSelection
+// scalafmt: { align.tokens = [{code = "<>"}, {code = "="}, {code = "=>"}, {code = ":="}]}
+
+class NamedSelectionSpec extends StageSpec:
+  test("Named selection multiple references") {
+    class ID extends DFDesign:
+      val x = UInt(16) <> IN
+      val y = Bits(8)  <> OUT
+      if (x > 5)
+        y := (x + 1).bits(7, 0) | (x + 1).bits(15, 8)
+      else
+        y := (x + 1).bits(15, 8)
+      y   := (x + 2).bits(11, 4)
+      if (x < 5)
+        y := (x + 2).bits(7, 0) | (x + 2).bits(15, 8)
+      else
+        y := (x + 2).bits(15, 8)
+
+    val id = (new ID).namedSelection
+    assertCodeString(
+      id,
+      """|class ID extends DFDesign:
+         |  val x = UInt(16) <> IN
+         |  val y = Bits(8) <> OUT
+         |  if (x > d"16'5")
+         |    val y_part = (x + d"16'1").bits
+         |    y := y_part(7, 0) | y_part(15, 8)
+         |  else
+         |    val y_part = (x + d"16'1").bits
+         |    y := y_part(15, 8)
+         |  val y_part = (x + d"16'2").bits
+         |  y := y_part(11, 4)
+         |  if (x < d"16'5") y := y_part(7, 0) | y_part(15, 8)
+         |  else y := y_part(15, 8)
+         |end ID
+         |""".stripMargin
+    )
+  }
+end NamedSelectionSpec
