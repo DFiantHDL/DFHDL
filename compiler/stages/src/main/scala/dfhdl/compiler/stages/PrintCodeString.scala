671c1ccbe65993b584bdeee24e917b9af87c8ae6
oronpo <oron.port@cornell.edu>
2023-10-05 16:42:57 +0300

    redundant code

--- compiler/stages/src/main/scala/dfhdl/compiler/stages/PrintCodeString.scala
+++ compiler/stages/src/main/scala/dfhdl/compiler/stages/PrintCodeString.scala
@@ -5,32 +5,6 @@ import dfhdl.compiler.printing.*
 import dfhdl.options.{CompilerOptions, PrinterOptions}
 
 case object PrintCodeString extends Stage:
-  object Coloring:
-    import io.AnsiColor._
-    val LIT: String = "\u001B[38;5;5m"
-    val scalaKWColor: String = s"$BLUE$BOLD"
-    val scalaKW: Set[String] =
-      Set("class", "end", "enum", "extends", "new", "object", "val", "if", "else", "match", "case")
-    val dfhdlKWColor: String = s"$MAGENTA$BOLD"
-    val dfhdlKW: Set[String] =
-      Set("VAR", "REG", "WIRE", "IN", "OUT", "INOUT", "VAL", "DFDesign", "RTDesign", "EDDesign",
-        "DFDomain", "RTDomain", "EDDomain", "process", "forever", "all")
-    val dfhdlOps: Set[String] = Set("<>", ":=", ":==")
-    val dfhdlTypes: Set[String] =
-      Set("Bit", "Boolean", "UInt", "SInt", "Bits", "X", "Encode", "Struct", "Opaque", "StartAt",
-        "OneHot", "Grey")
-    val dfhdlTPColor: String = "\u001B[38;5;94m"
-    val OP: String = s"$BOLD"
-    val FN: String = "\u001B[38;5;54m"
-    val TP: String = "\u001B[38;5;94m"
-    extension (text: String)
-      def color: String =
-        text
-          .colorWords(scalaKW, scalaKWColor)
-          .colorWords(dfhdlKW, dfhdlKWColor)
-          .colorOps(dfhdlOps, dfhdlKWColor)
-          .colorWords(dfhdlTypes, dfhdlTPColor)
-  end Coloring
   def dependencies: List[Stage] =
     List(DropUnreferencedAnons, NamedAnonMultiref, UniqueDesigns, DFHDLUniqueNames)
   def nullifies: Set[Stage] = Set()
