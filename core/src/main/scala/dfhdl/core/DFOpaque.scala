d6436383fa55225c00f0a0ca7d18de441f8e99e4
oronpo <oron.port@cornell.edu>
2023-10-07 01:44:37 +0300

    add experimental actualMap

--- core/src/main/scala/dfhdl/core/DFOpaque.scala
+++ core/src/main/scala/dfhdl/core/DFOpaque.scala
@@ -6,30 +6,30 @@ import scala.quoted.*
 
 import scala.annotation.unchecked.uncheckedVariance
 
-type DFOpaque[+T <: DFOpaque.Abstract] =
-  DFType[ir.DFOpaque, Args1[T @uncheckedVariance]]
+type DFOpaque[+TFE <: DFOpaque.Abstract] =
+  DFType[ir.DFOpaque, Args1[TFE @uncheckedVariance]]
 object DFOpaque:
   protected[core] sealed trait Abstract extends HasTypeName, ir.DFOpaque.CustomId:
     type ActualType <: DFTypeAny
     protected[core] val actualType: ActualType
 
-  abstract class Frontend[T <: DFTypeAny](final protected[core] val actualType: T) extends Abstract:
-    type ActualType = T
+  abstract class Frontend[A <: DFTypeAny](final protected[core] val actualType: A) extends Abstract:
+    type ActualType = A
 
-  given [T <: Abstract](using ce: ClassEv[T]): DFOpaque[T] = DFOpaque(ce.value)
+  given [TFE <: Abstract](using ce: ClassEv[TFE]): DFOpaque[TFE] = DFOpaque(ce.value)
 
-  def apply[T <: Abstract](
-      t: T
-  ): DFOpaque[T] =
-    ir.DFOpaque(t.typeName, t, t.actualType.asIR).asFE[DFOpaque[T]]
-  extension [T <: DFTypeAny, TFE <: Frontend[T]](dfType: DFOpaque[TFE])
-    def actualType: T = dfType.asIR.actualType.asFE[T]
+  def apply[TFE <: Abstract](
+      t: TFE
+  ): DFOpaque[TFE] =
+    ir.DFOpaque(t.typeName, t, t.actualType.asIR).asFE[DFOpaque[TFE]]
+  extension [A <: DFTypeAny, TFE <: Frontend[A]](dfType: DFOpaque[TFE])
+    def actualType: A = dfType.asIR.actualType.asFE[A]
 
-  type Token[T <: Abstract] = DFToken[DFOpaque[T]]
+  type Token[TFE <: Abstract] = DFToken[DFOpaque[TFE]]
   object Token:
-    def apply[T <: DFTypeAny, TFE <: Frontend[T]](
+    def apply[A <: DFTypeAny, TFE <: Frontend[A]](
         tfe: TFE,
-        token: T <> TOKEN
+        token: A <> TOKEN
     ): Token[TFE] =
       ir.DFToken(DFOpaque(tfe).asIR)(token.asIR.data).asTokenOf[DFOpaque[TFE]]
     def forced[TFE <: Abstract](
@@ -41,46 +41,46 @@ object DFOpaque:
     object TC:
       import DFToken.TC
       given DFOpaqueTokenFromDFOpaqueToken[
-          T <: Abstract,
+          TFE <: Abstract,
           RT <: Abstract,
           V <: DFOpaque[RT] <> TOKEN
-      ](using RT <:< T): TC[DFOpaque[T], V] with
-        def conv(dfType: DFOpaque[T], value: V)(using Ctx): Out =
-          value.asTokenOf[DFOpaque[T]]
+      ](using RT <:< TFE): TC[DFOpaque[TFE], V] with
+        def conv(dfType: DFOpaque[TFE], value: V)(using Ctx): Out =
+          value.asTokenOf[DFOpaque[TFE]]
 
     object Ops:
-      extension [T <: DFTypeAny, TFE <: Frontend[T]](
+      extension [A <: DFTypeAny, TFE <: Frontend[A]](
           lhs: DFOpaque[TFE] <> TOKEN
       )
-        def actual: T <> TOKEN =
+        def actual: A <> TOKEN =
           val lhsIR = lhs.asIR.asInstanceOf[ir.DFToken[ir.DFOpaque]]
-          ir.DFToken.forced(lhsIR.dfType.actualType, lhsIR.data).asTokenOf[T]
+          ir.DFToken.forced(lhsIR.dfType.actualType, lhsIR.data).asTokenOf[A]
   end Token
 
   object Val:
     object TC:
       import DFVal.TC
       given DFOpaqueValFromDFOpaqueVal[
-          T <: Abstract,
+          TFE <: Abstract,
           RT <: Abstract,
           V <: DFOpaque[RT] <> VAL
-      ](using RT <:< T): TC[DFOpaque[T], V] with
-        def conv(dfType: DFOpaque[T], value: V)(using Ctx): Out =
-          value.asValOf[DFOpaque[T]]
+      ](using RT <:< TFE): TC[DFOpaque[TFE], V] with
+        def conv(dfType: DFOpaque[TFE], value: V)(using Ctx): Out =
+          value.asValOf[DFOpaque[TFE]]
 
     object Ops:
       extension [L](inline lhs: L)
         transparent inline def as[Comp <: AnyRef](tfeComp: Comp): Any = ${ asMacro[L, Comp]('lhs) }
-      private def asDFVector[T <: DFTypeAny](dfVals: Iterable[DFValOf[T]])(using
+      private def asDFVector[A <: DFTypeAny](dfVals: Iterable[DFValOf[A]])(using
           DFC
-      ): DFValOf[DFVector[T, Tuple1[Int]]] =
+      ): DFValOf[DFVector[A, Tuple1[Int]]] =
         val dfType = DFVector(dfVals.head.dfType, Tuple1(dfVals.size))
         DFVal.Func(dfType, DFVal.Func.Op.++, dfVals.toList)(using dfc.anonymize)
-      extension [T <: DFTypeAny](lhs: Iterable[DFValOf[T]])
+      extension [A <: DFTypeAny](lhs: Iterable[DFValOf[A]])
         transparent inline def as[Comp <: AnyRef](
             tfeComp: Comp
         )(using DFC): Any = ${
-          asMacro[DFValOf[DFVector[T, Tuple1[Int]]], Comp]('{ asDFVector(lhs) })
+          asMacro[DFValOf[DFVector[A, Tuple1[Int]]], Comp]('{ asDFVector(lhs) })
         }
       private def asMacro[L, Comp <: AnyRef](
           lhs: Expr[L]
@@ -88,8 +88,8 @@ object DFOpaque:
         import quotes.reflect.*
         val tfeTpe = TypeRepr.of[Comp].getCompanionClassTpe
         tfeTpe.baseType(TypeRepr.of[Frontend[_ <: DFTypeAny]].typeSymbol) match
-          case AppliedType(_, tTpe :: _) =>
-            val tType = tTpe.asTypeOf[DFTypeAny]
+          case AppliedType(_, aTpe :: _) =>
+            val aType = aTpe.asTypeOf[DFTypeAny]
             val tfeType = tfeTpe.asTypeOf[Abstract]
             val tfe = '{
               compiletime
@@ -100,7 +100,7 @@ object DFOpaque:
             val lhsTpe = lhsTerm.tpe
             val lhsExpr = lhsTerm.asExpr
             val lhsType = lhsTpe.asTypeOf[Any]
-            val tExpr = '{ $tfe.actualType.asInstanceOf[tType.Underlying] }
+            val aExpr = '{ $tfe.actualType.asInstanceOf[aType.Underlying] }
             def hasDFVal(tpe: TypeRepr): Boolean =
               tpe.asTypeOf[Any] match
                 case '[DFValAny] => true
@@ -109,12 +109,12 @@ object DFOpaque:
                 case _ => false
             if (hasDFVal(lhsTpe))
               '{
-                val tc = compiletime.summonInline[DFVal.TC[tType.Underlying, lhsType.Underlying]]
+                val tc = compiletime.summonInline[DFVal.TC[aType.Underlying, lhsType.Underlying]]
                 val ctx = compiletime.summonInline[tc.Ctx]
                 trydf {
                   DFVal.Alias.AsIs(
                     DFOpaque[tfeType.Underlying]($tfe),
-                    tc($tExpr, $lhsExpr)(using ctx),
+                    tc($aExpr, $lhsExpr)(using ctx),
                     Token.forced[tfeType.Underlying]($tfe, _)
                   )(using ctx)
                 }(using ctx)
@@ -122,8 +122,8 @@ object DFOpaque:
             else
               '{
                 val tc =
-                  compiletime.summonInline[DFToken.TC[tType.Underlying, lhsType.Underlying]]
-                Token.forced[tfeType.Underlying]($tfe, tc($tExpr, $lhsExpr))
+                  compiletime.summonInline[DFToken.TC[aType.Underlying, lhsType.Underlying]]
+                Token.forced[tfeType.Underlying]($tfe, tc($aExpr, $lhsExpr))
               }
             end if
           case _ =>
@@ -131,13 +131,27 @@ object DFOpaque:
         end match
       end asMacro
 
-      extension [T <: DFTypeAny, TFE <: Frontend[T], A, C, I](
+      extension [AT <: DFTypeAny, TFE <: Frontend[AT], A, C, I](
           lhs: DFVal[DFOpaque[TFE], Modifier[A, C, I]]
       )
-        def actual(using DFC): DFVal[T, Modifier[A, Any, Any]] = // trydf {
+        def actual(using DFC): DFVal[AT, Modifier[A, Any, Any]] = // trydf {
           import Token.Ops.{actual => actualToken}
           DFVal.Alias.AsIs(lhs.dfType.actualType, lhs, _.actualToken)
 //        }
+        // TODO: what to do with the token function?
+        // TODO: there is strange result when applying `lhs.actualMap: lhs =>` with the same name
+        //       could be a scala compiler bug or a plugin bug (simple examples seem to work fine)
+        //       see https://scastie.scala-lang.org/PPXZD3rORxaxQ3PTmin6wg
+        @scala.annotation.experimental
+        def actualMap(
+            f: DFValOf[AT] => DFValOf[AT]
+        )(using dfc: DFC, ce: ClassEv[TFE]): DFValOf[DFOpaque[TFE]] =
+          DFVal.Alias.AsIs(
+            DFOpaque[TFE](ce.value),
+            f(lhs.actual),
+            x => ???
+          )
+      end extension
     end Ops
   end Val
 
