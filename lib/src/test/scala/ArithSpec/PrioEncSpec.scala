3ee09408262bcfd03040b40754ec1bab4d7273c7
oronpo <oron.port@cornell.edu>
2023-11-21 16:47:11 +0200

    verify returned width of priority encoder

--- lib/src/test/scala/ArithSpec/PrioEncSpec.scala
+++ lib/src/test/scala/ArithSpec/PrioEncSpec.scala
@@ -7,12 +7,12 @@ class PrioEncSpec extends DFSpec:
   test("Printing Encoder-8"):
     // TODO: this needs to be fixed. The name should propagate through the inline method prioEnc
     assertCodeString(
-      """|val b8 = Bits(8) <> VAR
-         |val ret = prioEncRecur(b8)
+      """|val b32 = Bits(32) <> VAR
+         |val ret = prioEncRecur(b32)
          |""".stripMargin
     ) {
-      val b8 = Bits(8) <> VAR
-      val res = prioEnc(b8)
-
+      val b32 = Bits(32) <> VAR
+      val res = prioEnc(b32)
+      res.width.verifyInlined(6)
     }
 end PrioEncSpec
