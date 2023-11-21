677b8b5bd49940deaaaaee565594220027471e14
oronpo <oron.port@cornell.edu>
2023-11-21 15:44:17 +0200

    add missing CanEqual for HWAnnotation

--- compiler/ir/src/main/scala/dfhdl/compiler/ir/HWAnnotation.scala
+++ compiler/ir/src/main/scala/dfhdl/compiler/ir/HWAnnotation.scala
@@ -9,6 +9,8 @@ abstract class HWAnnotation(when: Boolean) extends StaticAnnotation with HasType
   def isActive: Boolean = when
   def codeString(using Printer): String = this.typeName
 
+given CanEqual[HWAnnotation, HWAnnotation] = CanEqual.derived
+
 extension (annotList: List[Annotation])
   def getActiveHWAnnotations: List[HWAnnotation] = annotList.collect {
     case annot: HWAnnotation if annot.isActive => annot
