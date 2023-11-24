434c26daa551abea82204061cb66a12b58e96043
oronpo <oron.port@cornell.edu>
2023-11-24 22:07:54 +0200

    add test for Scala tuple naming

--- core/src/test/scala/CoreSpec/DFTupleSpec.scala
+++ core/src/test/scala/CoreSpec/DFTupleSpec.scala
@@ -29,6 +29,8 @@ class DFTupleSpec extends DFSpec:
        |val t11 = !t2._1._2
        |val t12 = t9(0)
        |val t13 = (d"8'8", 1)
+       |val t14 = t1._1
+       |val t15 = t2._1
        |""".stripMargin
   ) {
     val t1: (UInt[8], Bit, Bits[3]) <> VAL =
@@ -53,6 +55,7 @@ class DFTupleSpec extends DFSpec:
     val t11: Bit <> VAL = !t2._1._2
     val t12: UInt[8] <> VAL = t9(0)
     val t13: (UInt[8], Bit) <> VAL = (8, 1)
+    val (t14, t15) = (t1(0), t2._1)
     // TODO: this needs to work
     // val t14: (Bits[Int], Bit) <> VAL = (all(0), 1)
   }
