173ac8e5b4affd435da3cb7031eee0eb656199c5
oronpo <oron.port@cornell.edu>
2023-11-29 09:51:11 +0200

    priority encoder recur inside of if

--- lib/src/main/scala/dfhdl/arith/prioEnc.scala
+++ lib/src/main/scala/dfhdl/arith/prioEnc.scala
@@ -8,9 +8,9 @@ def prioEncRecur(value: Bits[Int] <> VAL): Bits[Int] <> DFRET =
   val width = value.width
   if (width == 1) value
   else
-    val lPrio = prioEncRecur(value.msbits(width / 2))
-    val rPrio = prioEncRecur(value.lsbits(width - width / 2))
-    val selPrio = if (value.msbit) lPrio else rPrio
+    val selPrio =
+      if (value.msbit) prioEncRecur(value.msbits(width / 2))
+      else prioEncRecur(value.lsbits(width - width / 2))
     (value.msbit, selPrio)
 
 @inline def prioEnc[W <: Int](value: Bits[W] <> VAL)(using
