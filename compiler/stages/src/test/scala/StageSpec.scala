a646e3a816a422b461bf0f8c9d08643ef8794dca
oronpo <oron.port@cornell.edu>
2023-12-14 10:43:42 +0200

    always conduct sanity checks in StageSpec

--- compiler/stages/src/test/scala/StageSpec.scala
+++ compiler/stages/src/test/scala/StageSpec.scala
@@ -1,12 +1,14 @@
 package dfhdl
 import dfhdl.compiler.ir.DB
 import dfhdl.compiler.printing.DefaultPrinter
+import dfhdl.compiler.stages.sanityCheck
 import dfhdl.internals.AllowTopLevel
 import munit.*
 
 abstract class StageSpec extends FunSuite, AllowTopLevel:
   def assertCodeString(db: DB, cs: String): Unit =
     import db.getSet
+    db.sanityCheck
     assertNoDiff(DefaultPrinter.csDB, cs)
 
 end StageSpec
