58b6b414f15057c6124cebc2fb52dbe9bfa4f370
oronpo <oron.port@cornell.edu>
2024-05-02 22:31:13 +0300

    docs: better scastie coloring

--- docs/include/scastie_hook.py
+++ docs/include/scastie_hook.py
@@ -1,15 +1,17 @@
 # Adds `in-browser.css` style for all `in-browser/` pages
 def inject_css_on_specific_pages(page, config, files, **kwargs):
     # Check if the page URL includes the 'in-browser/' directory
+    cssPath = "css/in-browser.css"
     if "in-browser/" in page.url:
         # Define the custom CSS to be included
-        custom_css_url = config["site_url"] + "css/in-browser.css"
         # Check if there's already extra CSS, append if there is, or create if there isn't
         if "extra_css" in config:
-            print(config["extra_css"])
-            config["extra_css"].append(custom_css_url)
+            if cssPath not in config["extra_css"]:
+                config["extra_css"].append(cssPath)
         else:
-            config["extra_css"] = [custom_css_url]
+            config["extra_css"] = [cssPath]
+    elif "extra_css" in config and cssPath in config["extra_css"]:
+        config["extra_css"].remove(cssPath)
 
 
 def on_page_context(context, page, config, nav):
