1ccbe3200a5777470810bdaab78d489eac9b8bde
oronpo <oron.port@cornell.edu>
2024-05-10 03:33:11 +0300

    add linting tests

--- lib/src/test/scala/issues/IssueSpec.scala
+++ lib/src/test/scala/issues/IssueSpec.scala
@@ -6,3 +6,9 @@ import dfhdl.*
 class IssuesSpec extends FunSuite:
   test("i116 compiles with no exception"):
     i116.GlobCounter(64).compile
+  test("i118 compiles and passes VHDL linting"):
+    given options.CompilerOptions.Backend = backends.vhdl
+    i118.ShiftIssue().compile.lint
+  test("i126 compiles and passes VHDL linting"):
+    given options.CompilerOptions.Backend = backends.vhdl
+    i126.TypeConvertIssue().compile.lint
