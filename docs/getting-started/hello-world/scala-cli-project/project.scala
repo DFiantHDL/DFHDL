065eb78bb11868ac3ea94a8cd116d9b723bf8f97
oronpo <oron.port@cornell.edu>
2024-05-10 04:08:32 +0300

    update version

--- docs/getting-started/hello-world/scala-cli-project/project.scala
+++ docs/getting-started/hello-world/scala-cli-project/project.scala
@@ -1,4 +1,4 @@
 //> using scala 3.4.0
-//> using dep io.github.dfianthdl::dfhdl::0.4.4
-//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.4.4
+//> using dep io.github.dfianthdl::dfhdl::0.4.6
+//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.4.6
 //> using option -deprecation -language:implicitConversions
--- docs/javascripts/scastie.js
+++ docs/javascripts/scastie.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let dfhdlVersion = "0.4.3";
+  let dfhdlVersion = "0.4.6";
   let scalaVersion = "3.4.0";
 
   let sbtConfig = `
