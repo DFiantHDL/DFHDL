77fabacbe9bc0e6c3c0fd67ff788244ca2fb6d35
oronpo <oron.port@cornell.edu>
2024-05-17 06:16:15 +0300

    issues will always trigger exception instead of exit

--- lib/src/test/scala/issues/IssueSpec.scala
+++ lib/src/test/scala/issues/IssueSpec.scala
@@ -5,6 +5,8 @@ import dfhdl.*
 import dfhdl.compiler.stages.getCompiledCodeString
 
 class IssuesSpec extends FunSuite:
+  given options.OnError = options.OnError.Exception
+
   test("i116 compiles with no exception"):
     i116.GlobCounter(64).compile
   test("i118 compiles and passes VHDL linting"):
