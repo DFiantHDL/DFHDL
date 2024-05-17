171a8b6263d5eccc6a7f3b701a6d7190b2ffdb48
oronpo <oron.port@cornell.edu>
2024-05-17 06:14:58 +0300

    prepare for Scala 3.5 plugin changes

---
+++ plugin/src/main/scala-3.5+/plugin/Plugin.scala
@@ -0,0 +1,20 @@
+package dfhdl.plugin
+
+import dotty.tools.dotc.plugins.*
+import dotty.tools.dotc.core.Contexts.Context
+
+class Plugin extends StandardPlugin:
+  val name: String = "dfhdl.plugin"
+  override val description: String = "Dedicated DSL capabilities for DFiant HDL"
+
+  override def initialize(options: List[String])(using Context): List[PluginPhase] =
+    val setting = new Setting(options.headOption)
+    MetaContextPlacerPhase(setting) ::
+      CustomControlPhase(setting) ::
+      DesignDefsPhase(setting) ::
+      MetaContextDelegatePhase(setting) ::
+      MetaContextGenPhase(setting) ::
+      OnCreateEventsPhase(setting) ::
+      FixInterpDFValPhase(setting) ::
+      Nil
+end Plugin
