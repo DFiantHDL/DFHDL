270a7df7f6ad5b52dfa8e80a58a01081a542280f
oronpo <oron.port@cornell.edu>
2024-07-30 21:54:01 +0300

    fix design filename collisions under windows due to case insensitivity

--- compiler/stages/src/main/scala/dfhdl/compiler/stages/UniqueDesigns.scala
+++ compiler/stages/src/main/scala/dfhdl/compiler/stages/UniqueDesigns.scala
@@ -16,17 +16,25 @@ case object UniqueDesigns extends Stage:
         (theseMembers lazyZip thoseMembers).forall { case (l, r) => l =~ r }
       case _ => false
     end eqDesign
+    // we're grouping always according to case-insensitive design names because these affect
+    // the eventual file names and we want these to be different across all operating systems.
+    // the actual name case is preserved for design/entity/module generation.
     val sameBlockLists: List[List[DFDesignBlock]] =
       designDB.uniqueDesignMemberList.view
-        .groupByCompare(eqDesign, _._1.dclName.hashCode()).map(_.unzip._1).toList
+        .groupByCompare(eqDesign, _._1.dclName.toLowerCase().hashCode()).map(_.unzip._1).toList
     val uniqueTypeMap: Map[String, List[List[DFDesignBlock]]] =
-      sameBlockLists.groupBy(g => g.head.dclName)
+      sameBlockLists.groupBy(g => g.head.dclName.toLowerCase())
     val patchList = uniqueTypeMap.flatMap { case (designType, list) =>
       list.zipWithIndex.flatMap {
         case (group, i) if group.length > 1 || list.length > 1 =>
+          val groupHead = group.head
+          // using the actual name and not the `designType` grouping, to preserve the original
+          // naming. we only lower-cased it for case-insensitive grouping.
           val updatedDclName =
-            if (list.length > 1) s"${designType}_${i.toPaddedString(list.length)}"
-            else designType
+            if (list.length > 1)
+              if (groupHead.isTop) groupHead.dclName // top name should not be mangled
+              else s"${groupHead.dclName}_${i.toPaddedString(list.length)}"
+            else groupHead.dclName
           var first = true
           group.map(block =>
             val tags =
