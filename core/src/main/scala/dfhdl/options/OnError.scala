71f5f550c5e560ea62ab3cded3125c76ea490034
oronpo <oron.port@cornell.edu>
2024-08-12 03:24:26 +0300

    prefer exceptions during sbt test as well

--- core/src/main/scala/dfhdl/options/OnError.scala
+++ core/src/main/scala/dfhdl/options/OnError.scala
@@ -5,6 +5,6 @@ enum OnError derives CanEqual:
   case Exit, Exception
 object OnError:
   private lazy val sbtShellIsRunning = getShellCommand match
-    case Some(cmd) if cmd.endsWith("xsbt.boot.Boot") => true
-    case _                                           => false
+    case Some(cmd) if cmd.endsWith("xsbt.boot.Boot") || cmd.endsWith("xsbt.boot.Boot test") => true
+    case _                                                                                  => false
   given OnError = if (sbtShellIsRunning) Exception else Exit
