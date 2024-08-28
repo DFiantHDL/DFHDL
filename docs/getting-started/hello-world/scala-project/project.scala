0be5bd2c17061ec1846589e7eeb52137463a1e29
oronpo <oron.port@cornell.edu>
2024-08-28 18:41:46 +0300

    docs: update to use v0.7.1 and utilize scastie printGenFiles default behavior

--- docs/getting-started/hello-world/scala-project/project.scala
+++ docs/getting-started/hello-world/scala-project/project.scala
@@ -1,4 +1,4 @@
 //> using scala 3.5.0
-//> using dep io.github.dfianthdl::dfhdl::0.7.0
-//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.7.0
+//> using dep io.github.dfianthdl::dfhdl::0.7.1
+//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.7.1
 //> using option -deprecation -language:implicitConversions
--- docs/getting-started/hello-world/scala-single-file/Counter8.scala
+++ docs/getting-started/hello-world/scala-single-file/Counter8.scala
@@ -1,6 +1,6 @@
 //> using scala 3.5.0
-//> using dep io.github.dfianthdl::dfhdl::0.7.0
-//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.7.0
+//> using dep io.github.dfianthdl::dfhdl::0.7.1
+//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.7.1
 //> using option -deprecation -language:implicitConversions
 
 import dfhdl.* //import all the DFHDL goodness
