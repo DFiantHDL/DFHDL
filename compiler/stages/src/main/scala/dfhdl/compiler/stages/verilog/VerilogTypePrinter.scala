06fbd16a61d095e1e03b4a81b325691cde5bf96a
oronpo <oron.port@cornell.edu>
2024-09-12 01:33:13 +0300

    turn enums into parameters under verilog.v95 and verilog.v2001

--- compiler/stages/src/main/scala/dfhdl/compiler/stages/verilog/VerilogTypePrinter.scala
+++ compiler/stages/src/main/scala/dfhdl/compiler/stages/verilog/VerilogTypePrinter.scala
@@ -30,19 +30,33 @@ protected trait VerilogTypePrinter extends AbstractTypePrinter:
       case (false, _) => ???
       case (true, _)  => ???
 
-  def csDFEnumTypeName(dfType: DFEnum): String = s"t_enum_${dfType.getName}"
+  val allowTypeDef: Boolean =
+    printer.dialect match
+      case VerilogDialect.v95 | VerilogDialect.v2001 => false
+      case _                                         => true
+  def csDFEnumTypeName(dfType: DFEnum): String =
+    if (allowTypeDef) s"t_enum_${dfType.getName}"
+    else csDFBits(DFBits(dfType.width), false)
   def csDFEnumDcl(dfType: DFEnum, global: Boolean): String =
     val enumName = dfType.getName
-    val entries =
+    if (allowTypeDef)
+      val entries =
+        dfType.entries.view
+          .map((n, v) => s"${enumName}_$n = $v")
+          .mkString(",\n")
+      // TODO: quartus seems to not accept an explicit size, so we drop it locally where it's not required.
+      // Globally, size is required (at least for verilator linter), so we need to drop enumeration altogether
+      // in such a case (change to a vector and list of constants) and then remove the special case handling
+      // here.
+      val explicitWidth = if (global) s" [${dfType.width - 1}:0]" else ""
+      s"typedef enum$explicitWidth {\n${entries.hindent}\n} ${csDFEnumTypeName(dfType)};"
+    else
       dfType.entries.view
-        .map((n, v) => s"${enumName}_$n = $v")
-        .mkString(",\n")
-    // TODO: quartus seems to not accept an explicit size, so we drop it locally where it's not required.
-    // Globally, size is required (at least for verilator linter), so we need to drop enumeration altogether
-    // in such a case (change to a vector and list of constants) and then remove the special case handling
-    // here.
-    val explicitWidth = if (global) s" [${dfType.width - 1}:0]" else ""
-    s"typedef enum$explicitWidth {\n${entries.hindent}\n} ${csDFEnumTypeName(dfType)};"
+        .map((n, v) => s"parameter ${enumName}_$n = $v;")
+        .mkString("\n")
+    end if
+  end csDFEnumDcl
+
   def csDFEnum(dfType: DFEnum, typeCS: Boolean): String = csDFEnumTypeName(dfType)
   def csDFVectorRanges(dfType: DFType): String =
     dfType match
