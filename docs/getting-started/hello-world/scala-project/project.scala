c4403f2fdb2bdca6f775ca605cd7954af9be4f0b
Oron Port <oron.port@dfiant.works>
2025-01-11 00:03:23 +0200

    update documentation to use Scala 3.5.2 and DFHDL v0.9.0

--- docs/getting-started/hello-world/scala-project/project.scala
+++ docs/getting-started/hello-world/scala-project/project.scala
@@ -1,4 +1,4 @@
-//> using scala 3.5.0
-//> using dep io.github.dfianthdl::dfhdl::0.8.0
-//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.8.0
+//> using scala 3.5.2
+//> using dep io.github.dfianthdl::dfhdl::0.9.0
+//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.9.0
 //> using option -deprecation -language:implicitConversions
