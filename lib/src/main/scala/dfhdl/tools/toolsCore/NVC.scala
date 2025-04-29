025a2126aba3906f377690f1303fae4e6296d5d0
Oron Port <oron.port@dfiant.works>
2025-04-29 15:40:59 +0300

    proper logging and error code for NVC

--- lib/src/main/scala/dfhdl/tools/toolsCore/NVC.scala
+++ lib/src/main/scala/dfhdl/tools/toolsCore/NVC.scala
@@ -56,6 +56,40 @@ object NVC extends VHDLLinter, VHDLSimulator:
     )
   end lintLogger
 
+  override protected def simulateLogger(using
+      CompilerOptions,
+      SimulatorOptions,
+      MemberGetSet
+  ): Option[Tool.ProcessLogger] =
+    val inVHDL93 =
+      summon[CompilerOptions].backend.asInstanceOf[backends.vhdl].dialect == VHDLDialect.v93
+    Some(
+      new Tool.ProcessLogger(
+        lineIsWarning = (line: String) => line.contains("** Warning:"),
+        lineIsSuppressed = (line: String) =>
+          // VHDL'93 does not have a standard finish, so we detect the DFHDL generated
+          // fatal report and convert it to the same behavior as in VHDL'2008 and later in NVC
+          if (inVHDL93)
+            if (line.endsWith(": Finished successfully (not an error)"))
+              // Extract the time and extra text from the line.
+              // The extra text is required because reports are asynchronous to regular printing
+              // in NVC, so they can appear at the start of the line.
+              val timePattern = """(.*)\*\* Failure\: (\d+ns)""".r
+              val matches = timePattern.findFirstMatchIn(line)
+              val time = matches.map(_.group(2)).get
+              val extra = matches.map(_.group(1)).get
+              println(s"$extra** Note: $time+1: FINISH called")
+              true
+            else false
+          else false,
+        lineIsErrorOpt =
+          if (inVHDL93)
+            Some((line: String) => line.contains("** Error:") || line.contains("** Failure:"))
+          else None
+      )
+    )
+  end simulateLogger
+
   override protected def lintCmdPostLangFlags(using
       CompilerOptions,
       ToolOptions,
