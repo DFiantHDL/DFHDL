edeb32cc8f940ae4a54be3aa0285183e657f3870
Scala Steward <scala_steward@virtuslab.com>
2025-06-13 15:17:34 +0000

    Update dfhdl to 0.11.4

--- docs/getting-started/hello-world/scala-project/project.scala
+++ docs/getting-started/hello-world/scala-project/project.scala
@@ -1,4 +1,4 @@
 //> using scala 3.7.1
-//> using dep io.github.dfianthdl::dfhdl::0.11.0
-//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.11.0
+//> using dep io.github.dfianthdl::dfhdl::0.11.4
+//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.11.4
 //> using option -deprecation -language:implicitConversions
