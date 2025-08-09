07d8d4406f700d3d8e27d8f7fcd9cfc771d68814
Oron Port <oron.port@dfiant.works>
2025-08-09 05:54:56 +0300

    add fill for IOBus resource

--- lib/src/main/scala/dfhdl/platforms/resources/VGA.scala
+++ lib/src/main/scala/dfhdl/platforms/resources/VGA.scala
@@ -1,21 +1,9 @@
 package dfhdl.platforms.resources
 
 class VGA() extends ResourceGroup:
-  val R0 = Sig()
-  val R1 = Sig()
-  val R2 = Sig()
-  val R3 = Sig()
-  val G0 = Sig()
-  val G1 = Sig()
-  val G2 = Sig()
-  val G3 = Sig()
-  val B0 = Sig()
-  val B1 = Sig()
-  val B2 = Sig()
-  val B3 = Sig()
+  val R = IOBus.fill(4)(Sig())
+  val G = IOBus.fill(4)(Sig())
+  val B = IOBus.fill(4)(Sig())
   val HS = Sig()
   val VS = Sig()
-  val R = IOBus(R0, R1, R2, R3)
-  val G = IOBus(G0, G1, G2, G3)
-  val B = IOBus(B0, B1, B2, B3)
 end VGA
