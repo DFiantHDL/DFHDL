7bcd52a5202ad79b28747f811feae09a4eb70a14
Oron Port <oron.port@dfiant.works>
2025-08-11 23:30:35 +0300

    refactor PmodDualConn classes to use val for member variables

--- lib/src/main/scala/dfhdl/platforms/pmods/PmodConn.scala
+++ lib/src/main/scala/dfhdl/platforms/pmods/PmodConn.scala
@@ -6,9 +6,9 @@ import dfhdl.DFC
 object PmodConn extends Connector.Companion(12)
 
 object PmodDualConn:
-  class Male(pm1: PmodConn.Male, pm2: PmodConn.Male) extends ResourceDeps:
+  class Male(val pm1: PmodConn.Male, val pm2: PmodConn.Male) extends ResourceDeps:
     lazy val upstreamDeps: List[Resource] = List(pm1, pm2)
-  class Female(pm1: PmodConn.Female, pm2: PmodConn.Female) extends ResourceDeps:
+  class Female(val pm1: PmodConn.Female, val pm2: PmodConn.Female) extends ResourceDeps:
     lazy val upstreamDeps: List[Resource] = List(pm1, pm2)
 
   given [M <: Male, F <: Female]: CanConnect[M, F] = (m, f) =>
