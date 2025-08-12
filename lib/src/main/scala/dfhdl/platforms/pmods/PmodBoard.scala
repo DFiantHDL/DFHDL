df08c1c0d2ca0507e813c10178b08475b4a99a74
Oron Port <oron.port@dfiant.works>
2025-08-12 13:50:07 +0300

    protect pmod connector values inside pmod board and create a direct connection

--- lib/src/main/scala/dfhdl/platforms/pmods/PmodBoard.scala
+++ lib/src/main/scala/dfhdl/platforms/pmods/PmodBoard.scala
@@ -1,8 +1,16 @@
 package dfhdl.platforms.pmods
 import dfhdl.platforms.Board
+import dfhdl.platforms.resources.*
+import Resource.CanConnect
 
-trait PmodBoard extends Board:
-  val pmodConn: PmodConn.Male
+trait PmodBoard extends Board, Resource:
+  protected val pmodConn: PmodConn.Male
+object PmodBoard:
+  given [B <: PmodBoard, FC <: PmodConn.Female]: CanConnect[B, FC] = (b, fc) =>
+    b.pmodConn <> fc
 
-trait PmodDualBoard extends Board:
-  val pmodDualConn: PmodDualConn.Male
+trait PmodDualBoard extends Board, Resource:
+  protected val pmodDualConn: PmodDualConn.Male
+object PmodDualBoard:
+  given [B <: PmodDualBoard, FC <: PmodDualConn.Female]: CanConnect[B, FC] = (b, fc) =>
+    b.pmodDualConn <> fc
