9f6723fcee56e1afe1b7e8394ec1bef216e818da
Oron Port <oron.port@dfiant.works>
2025-08-13 15:54:22 +0300

    generalized pmod connection

--- lib/src/main/scala/dfhdl/platforms/pmods/PmodBoard.scala
+++ lib/src/main/scala/dfhdl/platforms/pmods/PmodBoard.scala
@@ -6,11 +6,13 @@ import Resource.CanConnect
 trait PmodBoard extends Board, Resource:
   protected val pmodConn: PmodConn.Male
 object PmodBoard:
-  given [B <: PmodBoard, FC <: PmodConn.Female]: CanConnect[B, FC] = (b, fc) =>
-    b.pmodConn <> fc
+  given [B <: PmodBoard, R <: Resource](
+      using CanConnect[PmodConn.Male, R]
+  ): CanConnect[B, R] = (b, r) => b.pmodConn <> r
 
 trait PmodDualBoard extends Board, Resource:
   protected val pmodDualConn: PmodDualConn.Male
 object PmodDualBoard:
-  given [B <: PmodDualBoard, FC <: PmodDualConn.Female]: CanConnect[B, FC] = (b, fc) =>
-    b.pmodDualConn <> fc
+  given [B <: PmodDualBoard, R <: Resource](using
+      CanConnect[PmodDualConn.Male, R]
+  ): CanConnect[B, R] = (b, r) => b.pmodDualConn <> r
