2c9d598a517edfe396b0c164972613662c1418b3
Oron Port <oron.port@dfiant.works>
2025-08-17 00:29:36 +0300

    remove redundant file

--- lib/src/main/scala/dfhdl/platforms/devices/xilinxamd/series7/Device.scala
+++
@@ -1,9 +0,0 @@
-package dfhdl.platforms.devices.xilinxamd.series7
-import dfhdl.internals.HasTypeName
-import dfhdl.hw.constraints.*
-
-trait Device extends dfhdl.platforms.devices.Device:
-  val speedGrade: String
-  final lazy val vendor: deviceID.Vendor = deviceID.Vendor.XilinxAMD
-  final lazy val partName: String = s"$deviceName$packageName-$speedGrade"
-  final lazy val deviceVersion: String = ""
