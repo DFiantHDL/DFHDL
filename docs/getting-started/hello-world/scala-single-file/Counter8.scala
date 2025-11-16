1787dd002f0fa0cb3319bc4910190a72af8efe4f
Oron Port <oron.port@dfiant.works>
2025-11-17 00:14:18 +0200

    docs: Update Scala version to 3.7.4 and DFHDL dependencies to 0.16.0 across multiple files

--- docs/getting-started/hello-world/scala-single-file/Counter8.scala
+++ docs/getting-started/hello-world/scala-single-file/Counter8.scala
@@ -1,6 +1,6 @@
-//> using scala 3.7.1
-//> using dep io.github.dfianthdl::dfhdl::0.12.0
-//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.12.0
+//> using scala 3.7.4
+//> using dep io.github.dfianthdl::dfhdl::0.16.0
+//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.16.0
 //> using option -deprecation -language:implicitConversions
 
 import dfhdl.* //import all the DFHDL goodness
