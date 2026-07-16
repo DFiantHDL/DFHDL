51893dc0e5bee53063cafb8a5400ccca14b37553
Oron Port <oron.port@dfiant.works>
2026-07-16 15:16:36 +0300

    workaround for https://github.com/scala/scala3/issues/26550

--- core/src/main/scala/dfhdl/core/DFBoolOrBit.scala
+++ core/src/main/scala/dfhdl/core/DFBoolOrBit.scala
@@ -262,7 +262,8 @@ type DFConstBit = DFConstOf[DFBit]
 //BitNumWrapper is a wrapper for BitNum to preserve 0 or 1 values in basic operations
 //The type is also used as `Bit` in the DFHDL frontend, to allow using BitNum values in DFHDL code
 //and constructing DFBit DFHDL valeu types such as `Bit <> CONST`.
-into sealed class BitNumWrapper(val value: BitNum) extends AnyVal derives CanEqual:
+//TODO: implemented workaround for https://github.com/scala/scala3/issues/26550
+into sealed class BitNumWrapper(val value: Int) extends AnyVal derives CanEqual:
   def unary_! : BitNumWrapper = BitNumWrapper(if value == 0 then 1 else 0)
   def unary_~ : BitNumWrapper = unary_!
   def |(rhs: BitNumWrapper): BitNumWrapper =
@@ -281,4 +282,5 @@ object BitNumWrapper:
   given [T <: Int & Singleton](using T <:< BitNum): Conversion[T, BitNumWrapper] =
     x => BitNumWrapper(x.asInstanceOf[BitNum])
   given CanEqual[BitNumWrapper, BitNum] = CanEqual.derived
-  implicit def toBitNum(wrapper: BitNumWrapper): BitNum = wrapper.value
+  // TODO: implemented workaround for https://github.com/scala/scala3/issues/26550
+  implicit def toBitNum(wrapper: BitNumWrapper): BitNum = wrapper.value.asInstanceOf[BitNum]
