0ee77931ea35ae3c41633b467634481e5f85b496
Oron <oron.port@cornell.edu>
2026-08-14 23:20:46 +0300

    add missing BitsHL print coloring

--- compiler/ir/src/main/scala/dfhdl/compiler/printing/Printer.scala
+++ compiler/ir/src/main/scala/dfhdl/compiler/printing/Printer.scala
@@ -950,7 +950,7 @@ class DFPrinter(using val getSet: MemberGetSet, val printerOptions: PrinterOptio
   val dfhdlTypes: Set[String] = Set(
     "Bit", "Boolean", "Int", "UInt", "SInt", "Bits", "X", "Encoded", "Struct", "Opaque",
     "StartAt", "OneHot", "Gray", "Unit", "Time", "Freq", "String", "Double", "fs", "ns", "ps", "us",
-    "ms", "sec", "min", "hr", "Hz", "KHz", "MHz", "GHz"
+    "ms", "sec", "min", "hr", "Hz", "KHz", "MHz", "GHz", "BitsHL"
   )
   def colorCode(cs: String): String =
     cs
