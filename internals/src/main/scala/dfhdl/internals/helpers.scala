b69b79f8a906f5f61a711e798684e16d111501a0
Oron <oron.port@cornell.edu>
2026-08-14 18:20:05 +0300

    remove debug

--- internals/src/main/scala/dfhdl/internals/helpers.scala
+++ internals/src/main/scala/dfhdl/internals/helpers.scala
@@ -397,7 +397,6 @@ lazy val sbtnIsRunning: Boolean =
   sbtIsRunning && getShellCommand.exists(cmd => cmd.endsWith("--server") || cmd.endsWith("--detach-stdio"))
 
 lazy val sbtShellIsRunning: Boolean =
-  println(s"getShellCommand: ${getShellCommand}")
   getShellCommand.exists(cmd => cmd.endsWith("xsbt.boot.Boot") || cmd.endsWith("sbt-launch.jar"))
 
 lazy val sbtTestIsRunning: Boolean =
