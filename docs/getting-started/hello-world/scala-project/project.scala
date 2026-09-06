6bf291b86c22f771d26e0519875e8d77ea492c31
Oron <oron.port@cornell.edu>
2026-09-06 18:57:47 +0300

    docs: update to v0.23.2

--- docs/getting-started/hello-world/scala-project/project.scala
+++ docs/getting-started/hello-world/scala-project/project.scala
@@ -1,3 +1,3 @@
 //> using scala 3.8.4
-//> using dep io.github.dfianthdl::dfhdl::0.23.1
-//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.23.1
+//> using dep io.github.dfianthdl::dfhdl::0.23.2
+//> using plugin io.github.dfianthdl:::dfhdl-plugin:0.23.2
